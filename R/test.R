2421da369e0e3c826a536ecad9ccc98ec65ea876
Januar Harianto <januar.harianto@sydney.edu.au>
2017-07-03 16:00:13 +1000

    added new functions
    prepData();
    hasNA();
    monotonic();
    dupes();
    evenSpaced();

--- R/test.R
+++ R/test.R
@@ -1,8 +1,6 @@
 # Load my usual packages and functions
 source("~/Dropbox/R/functions/allFunctions.R")
 loadPackage("tidyverse")  # better code
-loadPackage("lubridate")  # handles datetime
-loadPackage("ggplot2")    # grammar of graphics (plots)
 loadPackage("cowplot")    # scientific theme for ggplot2
 loadPackage("viridis")    # colour scheme for ggplot2
 
@@ -14,118 +12,168 @@ loadPackage("viridis")    # colour scheme for ggplot2
 loadPackage("LoLinR")
 
 # Load data
-df <- read_csv("data/hery.csv") %>%
-  select(time=1,o2=4) %>%
+df <- readr::read_csv("data/hery.csv") %>%
+  dplyr::select(time=1,o2=4) %>%
   print()
-#
+df
+
 # Workflow ---------------------------------------------------------------------
-
-start <- 7
-end <- 30
-# :oad and  subset data
-full <- dplyr::rename_(df, x = names(df)[1], y = names(df)[2])
-win  <- dplyr::filter(full, x >= start, x <= end)
-# perform regression
-reg  <- lm(y ~ x, win)
-
-# Grab coefficients
-slope     <- round(coef(reg)[2], 4)
-intercept <- round(coef(reg)[1], 4)
-rsq       <- round(summary(reg)$r.squared, 3)
-arsq      <- round(summary(reg)$adj.r.squared, 3)
-
-# Generate tables
-vrs <- matrix(c(start, end), ncol = 2, byrow = T) %>%
-  `colnames<-`(c("Start", "End")) %>%
-  `rownames<-`("Time") %>%
-  as.table() %>% print()
-
-cat('\n')
-
-out <- matrix(c(slope, intercept, rsq, arsq),ncol = 4, byrow = F) %>%
-  `colnames<-`(c("Slope", "Intercept", "R-sq.", "Adj. R-sq")) %>%
-  `rownames<-`("") %>%
-  as.table() %>% print()
-
-# Plot
-plotFull <- ggplot(full, aes(x, y)) +
-  geom_point() +
-  geom_point(data = win, aes(x ,y), colour = "yellow")
-plotWin <- ggplot(win, aes(x, y)) +
-  geom_point() +
-  geom_label(data = subset(win, x == min(x) | x == max(x)),
-            aes(x, y, label = x))
-plot_grid(plotFull, plotWin, nrow = 2)
-
 
 # prepData function ------------------------------------------------------------
 
 prepData <- function(df) {
   df <- dplyr::rename_(df, x = names(df)[1], y = names(df)[2])
+
+  # Grab units
+  o2time <- as.character(readline("Unit of time (e.g. s, min):"))
+  o2unit <- as.character(readline("Unit of O2 conc, (e.g. mgL-1, ug/kg:"))
+  # save values
+  resprdf <<- df %>% print()
+  o2time <<- o2time
+  o2unit <<- o2unit
   # Plot
   ggplot(df, aes(x, y)) + geom_point()
-
-  rtime <- as.character(readline("Please specify unit of time, e.g. s, min, h:"))
-  o2unit <- as.character(readline("Please specify O2 unit (e.g. mgL-1, ug/kg:"))
-  # spit out values
-  resprdf <<- df
-  rtime <<- rtime
-  o2unit <<- o2unit
-  df
+  message("Table generated. Please run calcRO2() to process data.")
 }
-
-getRate <- function(start, end, df = resprdf) {
-  win  <- dplyr::filter(df, x >= start, x <= end)
-  win
-}
-
 
 prepData(df)
-getRate(10,50)
+resprdf
+# calcRate function -------------------------------------------------------------
 
-# getRate function -------------------------------------------------------------
-
-getRate <- function(start, end, df = resprdf) {
+calcRO2 <- function(start, end, df = resprdf) {
   # load andsubset data
+  df <- resprdf
   win  <- dplyr::filter(df, x >= start, x <= end)
   # perform regression
   reg  <- lm(y ~ x, win)
-  reg
-
   # Grab coefficients
   slope     <- round(coef(reg)[2], 4)
   intercept <- round(coef(reg)[1], 4)
-  rsq       <- round(summary(reg)$r.squared, 3)
-  arsq      <- round(summary(reg)$adj.r.squared, 3)
-
+  rsq       <- round(summary(reg)$r.squared, 5)
+  arsq      <- round(summary(reg)$adj.r.squared, 5)
+  # Delta method
+  dvars <- slice(win, c(1,n()))  # grab first and last row
+  dRO2  <- round(((dvars$y[2] - dvars$y[1]) / (dvars$x[2] - dvars$x[1])), 4)
   # Generate tables
-  vrs <- matrix(c(start, end), ncol = 2, byrow = T) %>%
-    `colnames<-`(c("Start", "End")) %>%
-    `rownames<-`("Time") %>%
-    as.table() %>% print()
-
   cat('\n')
-
-  out <- matrix(c(slope, intercept, rsq, arsq),ncol = 4, byrow = F) %>%
-    `colnames<-`(c("Slope", "Intercept", "R-sq", "Adj. R-sq")) %>%
+  out <- matrix(c(dRO2, slope, intercept, rsq, arsq,
+                  dvars$x[1], dvars$x[2]), ncol = 7, byrow = F) %>%
+    `colnames<-`(c("Delta", "Slope", "Intercept", "R-sq", "Adj R-sq",
+                   "Time start", "Time end")) %>%
     `rownames<-`("") %>%
-    as.table() %>% print()
-
+    as.table() %>%
+    print()
+  # Copy to clipboard
+  clip <- pipe("pbcopy", "w")
+  write.table(out, file=clip, col.names = F, row.names = F,
+              sep = "\t", eol = "\r")
+  close(clip)
+  # cat('\n')
+  # vrs <- matrix(c(start, end), ncol = 2, byrow = T) %>%
+  #   `colnames<-`(c("Start", "End")) %>%
+  #   `rownames<-`("Time") %>%
+  #   as.table() %>% print()
   # Plot
-  plotFull <- ggplot(df, aes(x, y)) +
-              geom_point() +
-              geom_point(data = win, aes(x ,y), colour = "yellow")
-  plotWin <- ggplot(win, aes(x, y)) +
-             geom_point()
+  plotFull <- ggplot(df, aes(x, y)) + ggtitle("Full data") +
+                geom_point() +
+                geom_point(data = win, aes(x ,y), colour = "seagreen3") +
+                xlab("Time") +
+                ylab("O2")
+  plotWin <- ggplot(win, aes(x, y)) + ggtitle("Selected") +
+               geom_point(colour = "seagreen4") +
+               geom_smooth(method = 'lm', colour = "black", size = .5, se = F) +
+               xlab("Time") +
+               ylab("O2")
   plot_grid(plotFull, plotWin, nrow = 2)
 }
 
+# Test
+prepData(df)
+calcRO2(10.1, 30)
+
+
 # Others -----------------------------------------------------------------------
-getRate(10,30)
-
-# lolinr stuff
 
 
-#get RO2 stuff
 
+nonmon <- read_csv("data/non_monotonic.csv") %>% print()
+
+# Check for NA and NaN ---------------------------------------------------------
+# TRUE - there are NA values
+# FALSE - no NA values
+hasNA <- function(dt, summary = F) {
+  check <- any(is.na(dt))
+  if (check == T) {
+    check %>% print()
+  }
+  if (check == F) {
+    check %>% print()
+  }
+  if (summary == T) {
+    which(is.na(dt), arr.ind = T) %>% print()
+  }
+}
+
+# Test
+hasNA(nonmon, T)
+
+# Check for duplicate time -----------------------------------------------------
+?duplicated.array()
+anyDuplicated(nonmon[1])
+nonmon[duplicated(nonmon[,1]),]
+
+dupes <- function(df, summary = F) {
+  check <- duplicated(df[[1]])
+  if (check == T) {
+
+  }
+}
+duplicated(nonmon[[1]])
+which(duplicated(nonmon[[1]]) == T)
+
+# Check for monotonically increasing time --------------------------------------
+# Are all the values in x equal to the cumulative maximum of the same values?
+# https://stackoverflow.com/a/13094801
+
+monotonic <- function(df, summary = F) {
+  x <- df
+  x <- x[[1]]
+  check <- all(x == cummax(x))
+  if (check == T) {
+    check %>% print()
+  }
+  if (check == F) {
+    check %>% print()
+    which(diff(x) < 0) %>% print() # ID rows that return FALSE
+  }
+}
+
+# Test
+monotonic(nonmon)
+
+# Check for evenly spaced time -------------------------------------------------
+# https://stackoverflow.com/a/4752580
+
+evenSpaced <- function(df, col = 1, tol = .Machine$double.eps * 100,
+                       summary = F) {
+  x <- df
+  x <- diff(x[[col]])
+  cond <- range(x) / mean(x)
+  check <- isTRUE(all.equal(cond[1], cond[2], tolerance = tol))
+  if (check == T) {
+    check %>% print()
+  }
+  if (check == F) {
+    check %>% print()
+    # message("The following rows differ from the median interval time:")
+    which(x != median(x)) %>% print() # ID rows that return FALSE
+  }
+}
+
+# Test
+evenSpaced(nonmon, 1)
+evenSpaced(nonmon, 2)
+
+# RespirE
 getRO2closed(df, volume = 200, by = "time", start = 7, end = 30)
+datatest(df)
