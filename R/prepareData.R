7a52c51236ba793513f93a4201894d336210e1dd
Januar Harianto <januar.harianto@sydney.edu.au>
2017-07-10 22:29:39 +1000

    fixed output

---
+++ R/prepareData.R
@@ -0,0 +1,37 @@
+#' Check data for issues
+#'@export
+prepareData <- function(df, plot=F) {
+  names(df) <- c("x", "y")  # rename for better ID
+  # prompt user for input
+  # message("Input dataframe units:")
+  # o2time <<- as.character(readline("Unit of time (e.g. s, min):"))
+  # o2unit <<- as.character(readline("Unit of O2 conc, (e.g. mgL-1, ug/kg:"))
+  # Checks
+  # message("Performing tests:")
+  # cat("Checking for missing data...\n")
+  xNA    <- hasNA(df,1)
+  yNA    <- hasNA(df,2)
+  # cat("Checking time data for duplicates...\n")
+  dup    <- dupes(df)
+  # cat("Checking if O2 data are monotonic...\n")
+  mono   <- monotonic(df)
+  # cat("Checking if time data are not evenly spaced...\n")
+  spaced <- evenSpaced(df)
+  # Summary
+  message("Summary:")
+  cat("Missing values in time data (x):    ", xNA, "\n")
+  cat("Missing values in O2 data (y):      ", yNA,"\n")
+  cat("Duplicates in time data (x):        ", dup, "\n")
+  cat("O2 data (y)is monotonic throughout: ", mono, "\n")
+  cat("Time data (x) is evenly spaced:     ", spaced, "\n")
+if(dup == T)
+  stop('There are duplicate time values.
+    Functions require all time values to be unique.')
+# All checks passed
+  #results
+  # if (plot)
+  #   return(ggplot(df, aes(x, y)) + geom_point())
+  else
+  message("New dataframe generated.")
+  return(df)
+}
