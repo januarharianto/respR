dbb45c979df0e9dd1c1d89d6319ae7bf66b5db53
Januar Harianto <januar.harianto@sydney.edu.au>
2017-07-10 21:31:17 +1000

    reformatted code for readability

--- R/checkUnits.R
+++ R/checkUnits.R
@@ -28,21 +28,35 @@ checkUnits <- function(unit, type) {
   if(type == 2) {
     allUnits <- list(
       'pct %'   = c('%', 'percent', 'percentage', '%o2', '%O2'),
-      'ug/L'    = c('ug/L', 'ug/l', 'ugL-1', 'ugl-1', 'ug per liter', 'ug per litre', 'µg/L', 'µg/l', 'µgL-1', 'µgl-1', 'µg per liter', 'µg per litre'),
-      'mmol/L'  = c('mmol/L', 'mmol/l', 'mmolL-1,', 'mmoll-1', 'mmol per liter', 'mmol per litre'),
-      'umol/L'  = c('umol/L', 'umol/l', 'umolL-1', 'umoll-1', 'µmol/L', 'µmol/l', 'µmolL-1', 'µmoll-1', 'umol per litre', 'umol per liter', 'µmol per litre', 'µmol per liter'),
-      'mL/L'    = c('ml/L', 'mL/L', 'ml/l', 'mll-1', 'mLl-1', 'mLL-1', 'mlL-1', 'ml per l', 'mL per L', 'ml per L'),
-      'mg/L'    = c('mg/L', 'mg/l', 'mgL-1', 'mgl-1', 'mg per litre', 'mg per liter'),
+      'ug/L'    = c('ug/L', 'ug/l', 'ugL-1', 'ugl-1', 'ug per liter',
+                    'ug per litre', 'µg/L', 'µg/l', 'µgL-1', 'µgl-1',
+                    'µg per liter', 'µg per litre'),
+      'mmol/L'  = c('mmol/L', 'mmol/l', 'mmolL-1,', 'mmoll-1', 'mmol per liter',
+                    'mmol per litre'),
+      'umol/L'  = c('umol/L', 'umol/l', 'umolL-1', 'umoll-1', 'µmol/L',
+                    'µmol/l', 'µmolL-1', 'µmoll-1', 'umol per litre',
+                    'umol per liter', 'µmol per litre', 'µmol per liter'),
+      'mL/L'    = c('ml/L', 'mL/L', 'ml/l', 'mll-1', 'mLl-1', 'mLL-1', 'mlL-1',
+                    'ml per l', 'mL per L', 'ml per L'),
+      'mg/L'    = c('mg/L', 'mg/l', 'mgL-1', 'mgl-1', 'mg per litre',
+                    'mg per liter'),
       'mg/kg'   = c('mg/kg', 'mgkg-1', 'mg per kg'),
-      'ug/kg'   = c('ug/kg', 'ugkg-1', 'ug per kg', 'µg/kg', 'µgkg-1', 'µg per kg'),
+      'ug/kg'   = c('ug/kg', 'ugkg-1', 'ug per kg', 'µg/kg', 'µgkg-1',
+                    'µg per kg'),
       'mL/kg'   = c('ml/kg', 'mL/kg', 'mlkg-1', 'mLkg-1', 'ml per kg'),
-      'mmol/kg' = c('mmol/kg', 'mmol/Kg', 'mmolkg-1', 'mmolKg-1', 'mmol per kg', 'mmol per Kg'),
-      'umol/kg' = c('umol/kg', 'umol/Kg', 'umolkg-1,', 'umolKg-1', 'umol per kg', 'umol per Kg', 'µmol/kg', 'µmol/Kg', 'µmolkg-1,', 'µmolKg-1', 'µmol per kg', 'µmol per Kg'),
+      'mmol/kg' = c('mmol/kg', 'mmol/Kg', 'mmolkg-1', 'mmolKg-1', 'mmol per kg',
+                    'mmol per Kg'),
+      'umol/kg' = c('umol/kg', 'umol/Kg', 'umolkg-1,', 'umolKg-1',
+                    'umol per kg', 'umol per Kg', 'µmol/kg', 'µmol/Kg',
+                    'µmolkg-1,', 'µmolKg-1', 'µmol per kg', 'µmol per Kg'),
       'Torr'    = c('torr', 'TORR', 'Torr', 'Tor', 'tor'),
-      'hPa'     = c('hPa', 'hpa', 'Hpa', 'HPA', 'HPa', 'hectopascal', 'hpascal'),
+      'hPa'     = c('hPa', 'hpa', 'Hpa', 'HPA', 'HPa', 'hectopascal',
+                    'hpascal'),
       'kPa'     = c('kPa', 'kpa', 'Kpa', 'KPA', 'KPa', 'kilopascal', 'kpascal'),
-      'mmHg'    = c('mmHg', 'mm Hg', 'mmhg', 'mm hg', 'MMHG', 'MM HG', 'millimeter of mercury', 'mm mercury'),
-      'inHg'    = c('inHg', 'in Hg', 'inhg', 'in hg', 'INHG', 'IN HG', 'inch of mercury', 'inch mercury'))
+      'mmHg'    = c('mmHg', 'mm Hg', 'mmhg', 'mm hg', 'MMHG', 'MM HG',
+                    'millimeter of mercury', 'mm mercury'),
+      'inHg'    = c('inHg', 'in Hg', 'inhg', 'in hg', 'INHG', 'IN HG',
+                    'inch of mercury', 'inch mercury'))
   }
   if(type == 3) {
     allUnits <- list(
@@ -62,7 +76,6 @@ checkUnits <- function(unit, type) {
     chk <- sapply(chk, function(x) length(x) > 0)
     result <- any(chk == T)
     name <- names(chk)[which(chk)]
-    # convert
 
     # print list
     out <- c(result, name)
