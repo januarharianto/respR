58f27f9ca78be552dab39d266b567dfa1f1f358c
Januar Harianto <januar.harianto@sydney.edu.au>
2017-07-11 00:41:06 +1000

    works perfect now (I think)

--- R/storeUnits.R
+++ R/storeUnits.R
@@ -11,49 +11,45 @@
 #' storeUnits()
 #' storeUnits('min', 'mgl-1', 'l', 'kg')
 storeUnits <- function(Time, O2, Vol, Mass) {
-  # prompt user for input
-  if(missing(Time)) {
-  Time <- as.character(readline("Unit of time: "))
-  if(checkUnits(Time, 1)[1] == F) {
-    message("Time unit not valid, please re-enter.")
-    Time <- as.character(readline("Unit of time: "))
-    }
-  }
-  if(missing(O2)) {
-    O2 <- as.character(readline("Unit of O2 conc: "))
-    if(checkUnits(O2, 2)[1] == F) {
-      message("O2 unit not valid, please re-enter.")
-      O2 <- as.character(readline("Unit of O2 conc.: "))
-    }
-  }
-  if(missing(Vol)) {
-    Vol <- as.character(readline("Unit of volume :"))
-    if(checkUnits(Vol, 3)[1] == F) {
-      message("Vol unit not valid, please re-enter.")
-      Vol <- as.character(readline("Unit of volume:"))
-    }
-  }
-  if(missing(Mass)) {
-    Mass <- as.character(readline("Unit of mass :"))
-    if(checkUnits(Mass, 4)[1] == F) {
-      message("Mass unit not valid, please re-enter.")
-      Mass <- as.character(readline("Unit of mass:"))
-    }
-  }
-  # rmass <- as.character(readline("Unit of specimen mass (if used) "))
-  # Summary
+  # Prompt if no input
+  if(missing(Time)) Time <- .validateInput(1, "Unit of time: ")[2]
+  if(missing(O2)) O2 <- .validateInput(2, "Unit of O2 concentration: ")[2]
+  if(missing(Vol)) Vol  <- .validateInput(3, "Unit of volume: ")[2]
+  if(missing(Mass)) Mass <- .validateInput(4, "Unit of mass: ")[2]
+
+  # Validate input
+  if(checkUnits(Time, 1)[1] == F)
+    Time <- .validateInput(1, "Time unit invalid. Unit of time: ")[2]
+  if(checkUnits(O2, 2)[1] == F)
+    O2 <- .validateInput(2, "O2 unit invalid. Unit of O2 concentration: ")[2]
+  if(checkUnits(Vol, 3)[1] == F)
+    Vol  <- .validateInput(3, "Volume unit invalid. Unit of volume: ")[2]
+  if(checkUnits(Mass, 4)[1] == F)
+    Mass <- .validateInput(4, "Mass unit invalid. Unit of mass: ")[2]
+
   Time <- checkUnits(Time, 1)[2]
-  O2   <- checkUnits(O2, 2)[2]
-  Vol  <- checkUnits(Vol, 3)[2]
-  Mass <- checkUnits(Mass, 4)[2]
-  .runits <<- list(time = Time, o2 = O2, vol = Vol, mass = Mass)
-  out <- matrix(c(Time, O2, Vol, Mass), ncol = 4)
-  colnames(out) <- c('Time', 'O2', 'Vol', 'Mass')
-  rownames(out) <- 'Unit'
-  # unames <- c('Time', 'O2', 'Vol', 'Mass')
-  # units  <- c(Time, O2, Vol, Mass)
-  # out <- data.frame(unames, units)
+  O2 <- checkUnits(O2,2)[2]
+  Vol <- checkUnits(Vol,3)[2]
+  Mass <- checkUnits(Mass,4)[2]
+
+  # # Generate output
+  cat("Time unit:   ", Time,"\n")
+  cat("O2 unit:     ", O2,"\n")
+  cat("Volume unit: ", Vol,"\n")
+  cat("Mass unit:   ", Mass,"\n")
+  out <- list(time = Time, O2 = O2, volume = Vol, mass = Mass)
   message("Unit of measurements saved and will be used for the session.")
+  class(out) <- "storeUnits"
   return(out)
 }
 
+
+# sub-function?
+.validateInput <- function(grp, string) {
+  x <- as.character(readline(string))
+  x <- checkUnits(x, grp)
+  if(x[1] == F) {
+    x <- .validateInput(grp, string)
+  }
+  return(x)
+}
