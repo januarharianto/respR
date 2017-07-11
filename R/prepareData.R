12cf7985831e2ffe832413d1c8e3ae2251cef77d
Januar Harianto <januar.harianto@sydney.edu.au>
2017-07-11 23:35:28 +1000

    upgrades
    - the function now automatically removes empty or NA rows.

--- R/prepareData.R
+++ R/prepareData.R
@@ -1,12 +1,16 @@
 #' Check data for issues
 #'@export
-prepareData <- function(df, plot=F) {
+prepareData <- function(df, plot = F) {
   names(df) <- c("x", "y")  # rename for better ID
   # Checks
   # message("Performing tests:")
   # cat("Checking for missing data...\n")
-  xNA    <- hasNA(df,1)
-  yNA    <- hasNA(df,2)
+  xNA    <- hasNA(df, 1)
+  yNA    <- hasNA(df, 2)
+  if (xNA == T | yNA == T)  # automatically remove NAs. Shouldn't affect data?
+  df <- na.omit(df)
+  n <- length(attributes(df)$na.action)
+  m <- length(attributes(df)$row.names)
   # cat("Checking time data for duplicates...\n")
   dup    <- dupes(df)
   # cat("Checking if O2 data are monotonic...\n")
@@ -16,18 +20,18 @@ prepareData <- function(df, plot=F) {
   # Summary
   message("Summary:")
   cat("Missing values in time data (x):    ", xNA, "\n")
-  cat("Missing values in O2 data (y):      ", yNA,"\n")
+  cat("Missing values in O2 data (y):      ", yNA, "\n")
   cat("Duplicates in time data (x):        ", dup, "\n")
   cat("O2 data (y)is monotonic throughout: ", mono, "\n")
   cat("Time data (x) is evenly spaced:     ", spaced, "\n")
-if(dup == T)
-  stop('There are duplicate time values.
-    Functions require all time values to be unique.')
-# All checks passed
-  #results
-  # if (plot)
-  #   return(ggplot(df, aes(x, y)) + geom_point())
+  if (dup == T)
+    stop('There are duplicate time values.
+      Functions require all time values to be unique. Process aborted.')
+  if (xNA == T | yNA == T)
+    message(sprintf('%i rows removed due to missing values. ', n),
+      sprintf('Data reduced to %i ', m),
+      sprintf('rows (from %i).', m + n))
   else
-  message("New dataframe generated.")
+    message("New dataframe generated.")
   return(df)
 }
