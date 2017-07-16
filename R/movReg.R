c49fa97b0181c46e88b7c6ff9bf3fd2ac4024c60
Januar Harianto <januar.harianto@sydney.edu.au>
2017-07-16 16:11:43 +1000

    upgrades
    - fixed output to exclude Nas
    - added benchmark

--- R/movReg.R
+++ R/movReg.R
@@ -1,7 +1,10 @@
 #' Moving regression version 2
 #'
-#'@export
+#' @import RcppRoll
+#' @export
 movReg <- function(df, span = 0.1) {
+  message("Performing rolling regressions...")
+  benchStart <- Sys.time()  # grab current time (for simple benchmark)
   win   <- floor(span*NROW(df))  # compute window size (rounded down)
   x <- df[[1]]
   y <- df[[2]]
@@ -10,6 +13,12 @@ movReg <- function(df, span = 0.1) {
   xBar  <-  roll_mean(x, win)  # mean of x
   yBar  <-  roll_mean(y, win)  # mean of y
   xxBar <-  roll_mean(x * x, win)  # mean of x^2
-  b1 <- c(rep(NA, win - 1), (xyBar - xBar * yBar) / (xxBar - xBar * xBar))
+  # b1 <- c(rep(NA, win - 1), (xyBar - xBar * yBar) / (xxBar - xBar * xBar))
+  b1 <- (xyBar - xBar * yBar) / (xxBar - xBar * xBar)
+
+  benchEnd <- round(unclass(Sys.time() - benchStart)[1], 1)
+  message(sprintf("%d regressions fitted ", NROW(b1)),
+    sprintf("in %g seconds", benchEnd), "\n")
+
   return(b1)
 }
