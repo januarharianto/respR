da80b6e13061eb61ac57e4de8ffefa0358908ccc
Januar Harianto <januar.harianto@sydney.edu.au>
2017-07-24 00:07:18 +1000

    pcrit: minor fixes; no changes in functionality

--- R/pcrit.R
+++ R/pcrit.R
@@ -11,23 +11,20 @@
 pcrit <- function(df, span = 0.05, MR = FALSE) {
   if (MR == T) {
     mrDo <- df
-  } # end if
-
-  if(MR == F) {
+  }
+  if (MR == F) {
     names(df) <- c("x", "y")
     # convert time to integer, if necessary
     if (any(inherits(df$x, "POSIXct"), (inherits(df$x, "POSIXt"))) == T) {
       df$x   <- as.integer(df$x - df$x[1])
-    } # end if
-
+    }
     # otherwise, carry on
     width    <- floor(span * nrow(df))
     rollreg  <- movReg(df, span)
     rollmean <- RcppRoll::roll_mean(df[[2]], width)
     counts   <- length(rollmean) # for benchmark
     mrDo     <- data.frame(rollmean, abs(rollreg))
-  } # end if
-
+  }
   # calculate pcrit here
   names(mrDo) <- c('do', 'mr')
   mrDo        <- mrDo[order(mrDo$do), ]  # sort (ascending)
@@ -75,22 +72,22 @@ plot.pcrit <- function(x, rank = 1, ...) {
 
   p <-
     ggplot(data, aes(do, mr)) +
-      geom_line(size = 1, colour = 'navy') +
-      stat_smooth(data = lm1, aes(do, mr),  na.rm=TRUE, se = F, method = 'lm', fullrange = T, size = .25, linetype = 2, colour = 'black', alpha = .1) +
-      stat_smooth(data = lm2, aes(do, mr), na.rm=TRUE, se = F, method = 'lm', fullrange = T, size = .25, linetype = 2, colour = 'black', alpha = .1) +
-      stat_smooth(data = lm1, aes(do, mr),  na.rm=TRUE, se = F, method = 'lm', fullrange = F, size = .5, colour = 'firebrick') +
-      stat_smooth(data = lm2, aes(do, mr), na.rm=TRUE, se = F, method = 'lm', fullrange = F, size = .5, colour = 'firebrick') +
-      geom_vline(xintercept = x$pcritRanked[5][rank, ], size = .5, linetype = 3, colour = 'darkorchid2') + # pcrit intercept
-      geom_vline(xintercept = x$pcritRanked[6][rank, ], size = .5, linetype = 3, colour = 'coral2') + # pcrit midpoint
-      annotate('text', x = x$pcritRanked[5][rank, ], y = max(data$mr), label = signif(x$pcritRanked[5][rank, ], 3), angle = 90) +
-      annotate('text', x = x$pcritRanked[6][rank, ], y = min(data$mr), label = signif(x$pcritRanked[6][rank, ], 3), angle = 90) +
-      ylab('Metabolic rate') +
-      xlab('Oxygen concentration') +
-      scale_x_continuous(breaks = scales::pretty_breaks(n = 10)) +
-      scale_y_continuous(breaks = scales::pretty_breaks(n = 10), limits = c(0, 1.1 * max((data[2])))) +
-      # labs(colour = "Regression") +
-      theme_respr() +
-      geom_blank()
+    geom_line(size = 1, colour = 'navy') +
+    stat_smooth(data = lm1, aes(do, mr),  na.rm=TRUE, se = F, method = 'lm', fullrange = T, size = .25, linetype = 2, colour = 'black', alpha = .1) +
+    stat_smooth(data = lm2, aes(do, mr), na.rm=TRUE, se = F, method = 'lm', fullrange = T, size = .25, linetype = 2, colour = 'black', alpha = .1) +
+    stat_smooth(data = lm1, aes(do, mr),  na.rm=TRUE, se = F, method = 'lm', fullrange = F, size = .5, colour = 'firebrick') +
+    stat_smooth(data = lm2, aes(do, mr), na.rm=TRUE, se = F, method = 'lm', fullrange = F, size = .5, colour = 'firebrick') +
+    geom_vline(xintercept = x$pcritRanked[5][rank, ], size = .5, linetype = 3, colour = 'darkorchid2') + # pcrit intercept
+    geom_vline(xintercept = x$pcritRanked[6][rank, ], size = .5, linetype = 3, colour = 'coral2') + # pcrit midpoint
+    annotate('text', x = x$pcritRanked[5][rank, ], y = max(data$mr), label = signif(x$pcritRanked[5][rank, ], 3), angle = 90) +
+    annotate('text', x = x$pcritRanked[6][rank, ], y = min(data$mr), label = signif(x$pcritRanked[6][rank, ], 3), angle = 90) +
+    ylab('Metabolic rate') +
+    xlab('Oxygen concentration') +
+    scale_x_continuous(breaks = scales::pretty_breaks(n = 10)) +
+    scale_y_continuous(breaks = scales::pretty_breaks(n = 10), limits = c(0, 1.1 * max((data[2])))) +
+    # labs(colour = "Regression") +
+    theme_respr() +
+    geom_blank()
 
   return(p)
 }
