1f1f1e6c961f3df377db3c5f78ae7901440ab81b
Januar Harianto <januar.harianto@sydney.edu.au>
2017-07-23 10:55:40 +1000

    checkUnits: improvements
    - changed argument “type” from numeric to character, which makes it easier for us to know what we are checking for
    - updated the units database.
    - I should probably swap to regex at some point...

--- R/checkUnits.R
+++ R/checkUnits.R
@@ -2,7 +2,7 @@
 #' @description Checks input string against a known database and returns a
 #' logical output (i.e. TRUE or FALSE). If TRUE, also prints out the identified
 #' string. Works for time (e.g min, sec), unit of O2 measurement (e.g. %, mg/L),
-#' volume (e.g. mL, L) and mass/weight (e.g. kg).
+#' volume (e.g. mL, L) and weight/mass (e.g. kg).
 #' @param unit Character string. A unit of measurement.
 #' @param type Numeric. Identifies the type of unit to check. 1: time; 2: O~2~
 #' 3: volume; 4: mass.
@@ -14,65 +14,67 @@
 #' @return NULL
 #' @export
 #' @examples
-#' checkUnits('min', 1)
-#' checkUnits('notatunit', 1)
+#' checkUnits('min', 'time')
+#' checkUnits('notatunit', 'time')
 #'
-#' checkUnits('hpa', 2)
-#' checkUnits('notatunit', 2)
+#' checkUnits('hpa', 'o2')
+#' checkUnits('notatunit', 'o2')
 checkUnits <- function(unit, type) {
-  if (type == 1) {
+  # not sure if worth ID'ing some of these using regex (too many variations)
+  # ok it's worth it but I've come too far.... will fix in future version
+  # doing it the stupid way:
+  # time units
+  if (type == 'time') {
     allUnits <- list(
-      hour = c('hour', 'hr', 'h'),
-      min  = c('minute', 'min', 'm'),
-      sec  = c('second', 'sec', 's'))
+      hour.time = c('hour', 'hr', 'h'),
+      min.time  = c('minute', 'min', 'm'),
+      sec.time  = c('second', 'sec', 's'))
   }
-  if(type == 2) {
+  # 2-dimensional o2 units, and pressure
+  if(type == 'o2') {
     allUnits <- list(
-      '%'       = c('%', 'percent', 'percentage', '%o2', '%O2'),
-      'ug/L'    = c('ug/L', 'ug/l', 'ugL-1', 'ugl-1', 'ug per liter',
-                    'ug per litre', 'µg/L', 'µg/l', 'µgL-1', 'µgl-1',
-                    'µg per liter', 'µg per litre'),
-      'mmol/L'  = c('mmol/L', 'mmol/l', 'mmolL-1,', 'mmoll-1', 'mmol per liter',
-                    'mmol per litre'),
-      'umol/L'  = c('umol/L', 'umol/l', 'umolL-1', 'umoll-1', 'µmol/L',
-                    'µmol/l', 'µmolL-1', 'µmoll-1', 'umol per litre',
-                    'umol per liter', 'µmol per litre', 'µmol per liter'),
-      'mL/L'    = c('ml/L', 'mL/L', 'ml/l', 'mll-1', 'mLl-1', 'mLL-1', 'mlL-1',
-                    'ml per l', 'mL per L', 'ml per L'),
-      'mg/L'    = c('mg/L', 'mg/l', 'mgL-1', 'mgl-1', 'mg per litre',
-                    'mg per liter'),
-      'mg/kg'   = c('mg/kg', 'mgkg-1', 'mg per kg'),
-      'ug/kg'   = c('ug/kg', 'ugkg-1', 'ug per kg', 'µg/kg', 'µgkg-1',
-                    'µg per kg'),
-      'mL/kg'   = c('ml/kg', 'mL/kg', 'mlkg-1', 'mLkg-1', 'ml per kg'),
-      'mmol/kg' = c('mmol/kg', 'mmol/Kg', 'mmolkg-1', 'mmolKg-1', 'mmol per kg',
-                    'mmol per Kg'),
-      'umol/kg' = c('umol/kg', 'umol/Kg', 'umolkg-1,', 'umolKg-1',
-                    'umol per kg', 'umol per Kg', 'µmol/kg', 'µmol/Kg',
-                    'µmolkg-1,', 'µmolKg-1', 'µmol per kg', 'µmol per Kg'),
-      'Torr'    = c('torr', 'TORR', 'Torr', 'Tor', 'tor'),
-      'hPa'     = c('hPa', 'hpa', 'Hpa', 'HPA', 'HPa', 'hectopascal',
-                    'hpascal'),
-      'kPa'     = c('kPa', 'kpa', 'Kpa', 'KPA', 'KPa', 'kilopascal', 'kpascal'),
-      'mmHg'    = c('mmHg', 'mm Hg', 'mmhg', 'mm hg', 'MMHG', 'MM HG',
-                    'millimeter of mercury', 'mm mercury'),
-      'inHg'    = c('inHg', 'in Hg', 'inhg', 'in hg', 'INHG', 'IN HG',
-                    'inch of mercury', 'inch mercury'))
+      '%.o2'       = c('%.o2', '%', 'percent', 'percentage', '%o2', '%O2'),
+      'ug/L.o2'    = c('ug/L.o2','ug/L', 'ug/l', 'ug / L', 'ug / l', 'ugL-1', 'ugl-1', 'ug L-1', 'ug l -1', 'ug per liter', 'ug per litre'),
+      'mmol/L.o2'  = c('mmol/L.o2', 'mmol/L', 'mmol/l', 'mmol / L', 'mmol / l',
+                       'mmolL-1,', 'mmoll-1', 'mmol L-1,', 'mmol l-1', 'mmol per liter', 'mmol per litre'),
+      'umol/L.o2'  = c('umol/L.o2','umol/L', 'umol/l', 'umolL-1', 'umoll-1',
+                       'umol / L', 'umol / l', 'umol L-1', 'umol l-1', 'umol per litre', 'umol per liter'),
+      'mL/L.o2'    = c('mL/L.o2', 'ml/L', 'mL/L', 'mL/l', 'ml/l', 'mll-1', 'mLl-1', 'mLL-1', 'mlL-1',
+                       'ml / L', 'mL / L', 'mL / l', 'ml / l', 'ml l-1', 'mL l-1', 'mL L-1', 'ml L-1', 'ml per l', 'mL per L', 'ml per L'),
+      'mg/L.o2'    = c('mg/L.o2', 'mg/L', 'mg/l', 'mg / l', 'mg / L', 'mgL-1', 'mgl-1', 'mg L-1', 'mg l-1', 'mg per litre', 'mg per liter'),
+      'mg/kg.o2'   = c('mg/kg.o2', 'mg/kg', 'mg / kg', 'mgkg-1', 'mg kg-1', 'mg per kg'),
+      'ug/kg.o2'   = c('ug/kg.o2', 'ug/kg', 'ugkg-1', 'ug / kg', 'ug kg-1', 'ug per kg'),
+      'mL/kg.o2'   = c('mL/kg.o2', 'ml/kg', 'mL/kg', 'mlkg-1', 'mLkg-1', 'ml / kg', 'mL / kg', 'ml kg-1', 'mL kg-1', 'ml per kg'),
+      'mmol/kg.o2' = c('mmol/kg.o2', 'mmol/kg', 'mmol/Kg', 'mmolkg-1', 'mmolKg-1',
+                       'mmol / kg', 'mmol / Kg', 'mmol kg-1', 'mmol Kg-1', 'mmol per kg', 'mmol per Kg'),
+      'umol/kg.o2' = c('umol/kg.o2', 'umol/kg', 'umol/Kg', 'umolkg-1,', 'umolKg-1',
+                       'umol / kg', 'umol / Kg', 'umol kg-1,', 'umol Kg-1', 'umol per kg', 'umol per Kg'),
+      'Torr.o2p'   = c('Torr.o2p', 'torr', 'TORR', 'Torr', 'Tor', 'tor'),
+      'hPa.o2p'    = c('hPa.o2p', 'hPa', 'hpa', 'Hpa', 'HPA', 'HPa', 'hectopascal', 'hpascal'),
+      'kPa.o2p'    = c('kPa.o2p', 'kPa', 'kpa', 'Kpa', 'KPA', 'KPa', 'kilopascal', 'kpascal'),
+      'mmHg.o2p'   = c('mmHg.o2p', 'mmHg', 'mm Hg', 'mmhg', 'mm hg', 'MMHG', 'MM HG', 'millimeter of mercury', 'mm mercury'),
+      'inHg.o2p'   = c('inHg.o2p', 'inHg', 'in Hg', 'inhg', 'in hg', 'INHG', 'IN HG', 'inch of mercury', 'inch mercury'))
   }
-  if(type == 3) {
+  if(type == 'vol') {
     allUnits <- list(
-      uL = c('ul', 'uL', 'microlitre', 'microliter', 'micro litre',
-             'micro liter'),
-      mL = c('ml', 'mL', 'millilitre', 'milli litre', 'milliliter',
-             'milli liter'),
-      L  = c('l', 'L', 'liter', 'litre', 'Litre', 'Liter'))
+      uL.vol = c('ul.vol', 'ul', 'uL', 'microlitre', 'microliter', 'micro litre', 'micro liter'),
+      mL.vol = c('mL.vol', 'ml', 'mL', 'millilitre', 'milli litre', 'milliliter', 'milli liter'),
+      L.vol  = c('L.vol', 'l', 'L', 'liter', 'litre', 'Litre', 'Liter'))
   }
-  if(type == 4) {
+  if(type == 'mass') {
     allUnits <- list(
-      ug  = c('ug', 'UG', 'µg', 'ugram', 'microgram'),
-      mg  = c('mg', 'MG', 'mgram', 'milligram'),
-      g   = c('g', 'G', 'gram'),
-      kg  = c('kg', 'KG', 'kilogram', 'kgram'))
+      ug.mass  = c('ug.mass', 'ug', 'UG', 'µg', 'ugram', 'microgram'),
+      mg.mass  = c('mg.mass', 'mg', 'MG', 'mgram', 'milligram'),
+      g.mass   = c('g.mass', 'g', 'G', 'gram'),
+      kg.mass  = c('kg.mass', 'kg', 'KG', 'kilogram', 'kgram'))
+  }
+  if(type == 'o1') {
+    allUnits <-  list(
+    'mg.o2'   = c('mg.o2', 'mg', 'milligram'),
+    'ug.o2'   = c('ug.o2', 'ug', 'microgram'),
+    'mmol.o2' = c('mmol.o2', 'mmol', 'millimol'),
+    'umol.o2' = c('umol.o2', 'umol', 'micromol'),
+    'ml.o2'   = c('ml.o2', 'ml', 'mL', 'millil'))
   }
   # look for match
   unit <- paste0('^', unit, '$')  # for exact matching
