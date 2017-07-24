e6e3e413a6afc7e4a4313739d49398b02109c107
Januar Harianto <januar.harianto@sydney.edu.au>
2017-07-25 01:16:19 +1000

    misc_functions: removed from export

--- R/misc_funs.R
+++ R/misc_funs.R
@@ -1,79 +1,75 @@
-#' @title Wrapper for if-stop condition in functions.
-#' @description A very simple function that may not be used...
-#' @param condition The expression/condition to evaluate.
-#' @param msg An error message which will be printed if condition is fulfiled.
-#' @return NULL
-#' @author Januar Harianto
-#' @export
-#' @examples
-#' errorCheck(x > 5, "Value is bigger than 5.")
+
+# compact stopifnot method, if needed
 # credit to https://stackoverflow.com/a/8345314
-stop.since <- function (condition, msg) {
+if.stop <- function (condition, msg) {
   if (condition) stop(msg, call. = FALSE)
 }
 
-#' @title Check for NA and NaN
-#' @description Returns TRUE or FALSE. Also prints an index when summary = T.
-#' Dataframe MUST be parsed through prepData() before this.
-#' @param df Dataframe.
-#' @param col Numeric. Selects column index to parse.
-#' @param summary Logical. Defaults to FALSE.
-#' @return NULL
-#' @author Januar Harianto
-#' @export
-#' @examples
-#' hasNA
-# TRUE - there are NA values
-# FALSE - no NA values
-hasNA <- function(df, col=1, summary = F) {
-  x <- df[[col]]
-  check <- any(is.na(x))
-  out <- which(is.na(x))
-  if(summary)
-    return(out)
-  else
-    return(check)
+# check that x and y are of equal length
+# unequal.data <- data.frame(c(1:10), c(1:20))
+# check.length(unequal.data[[1]], unequal.data[[2]])
+check.length <- function(x, y) {
+  check <- all.equal(length(x), length(y))
+  id <- c(length(x), length(y))
+  out <- list(check = check, id = id)
+  return(out)
 }
 
+# check that data is numeric or time data, where appropriate
+numeric.datetime <- function(x, y) {
+  # check if x is numeric or time data
+  check.x <- is.numeric(x) | inherits(x, "POSIXct") | inherits(x, "POSIXt")
+  # check if y is numeric
+  check.y <- is.numeric(y)
+  out <- c(check.x, check.y)
+  return(out)
+}
 
+# check for NA values (also less specific NaN)
+# TRUE = has NA
+# test:
+# na.data <- data.frame(x = c(1:15), y = c(1:5, rep(NA, 3), 9:15))
+# has.NA(na.data)
+has.NA <- function(df) {
+  check <- any(is.na(df))
+  id <- which(is.na(df))
+  out <- list(check = check, id = id)
+  return(out)
+}
 
-#' @title Check for duplicates
-#' @author Januar Harianto
-#' @export
+# check for duplicated time
+# TRUE = has dupes in time
+# note: only checks time column, assumed to be first column
+# test:
+# dup.time.data <- data.frame(x = c(1:5, 1:5), y = c(1:10))
+# dupes(dup.time.data)
 dupes <- function(df, summary = F) {
   x      <- df[[1]]
   check  <- isTRUE(any(duplicated(x)))
-  out    <- which(duplicated(x) == T)
-  if(summary)
-    return(out)
-  else
-    return(check)
+  id    <- which(duplicated(x) == T)
+  out <- list(check = check, id = id)
+  return(out)
 }
 
-#' @title Check for monotonically increasing data.
-#' @author Januar Harianto
-#' @export
+
+# Check for monotonically increasing data. Not used (switch to Spearman's)
 # https://stackoverflow.com/a/13094801
-# Maybe expand this to detect monotically decreasing data as well. Then, it
-# might be implemented in a function to DETECT INTERMITTENT DATASET :D :D :D
-monotonic <- function(df, summary=F){
-  x <- df[[1]]
-  check <- all(x == cummax(x))
-  out   <- which(diff(x) < 0) # ID rows that return FALSE
-  if (summary)
-    return(out)
-  else
-    return(check)
+# test:
+# non.monotonic.data <- data.frame(x = c(1:4, 5:2, 1:7), y = c(1:15))
+# monotonic(non.monotonic.data)
+monotonic <- function(y, summary = F) {
+  check <- all(y == cummax(y)) | all(y == cummin(y))
+  out <- check
+  return(out)
 }
 
-#' @title Check for evenly spaced time data
-#' @author Januar Harianto
-#' @export
+# Check for evenly spaced data
 # https://stackoverflow.com/a/4752580
-evenSpaced <- function(df, col=1, tol=.Machine$double.eps * 100){
-  x     <- diff(df[[col]])
+even.spaced <- function(x, tol = .Machine$double.eps * 100){
+  x     <- diff(x)
   cond  <- range(x) / mean(x)
   check <- isTRUE(all.equal(cond[1], cond[2], tolerance = tol))
-  return(check)
-}
+  out <- check
+  return(out)
+  }
 
