34dc9b478f6f8463ceb4f58b4e3e626f17029502
Januar Harianto <januar.harianto@sydney.edu.au>
2017-08-17 14:36:08 +1000

    calc.bg.rate: added plot

--- R/calc.bg.rate.R
+++ R/calc.bg.rate.R
@@ -5,27 +5,40 @@
 #' @param bgcol Numeric or a list. Defaults to 2. The column index/indices of the data frame to use for DO data. A list e.g. `c(2:5)` will calculate the rate for each column individually, and then average the results.
 #'
 #' @return A summary list of rates, and the mean rate.
+#' @importFrom dplyr select
 #' @export
 #'
 #' @examples
 #' data(urchin2013)
-#' calc.background(urchin2013, 1, c(18:21))
+#' calc.bg.rate(urchin2013, 1, c(18:19))
+#' # save as object to use in other functions:
+#' urbg <- calc.bg.rate(urchin2013,1,c(18:19))
 #'
-calc.bg.rate <- function(df, timecol = 1, bgcol = 2) {
-  df <- df[, c(timecol, bgcol)]   # subset dataframe
-  n <- c(2:length(df)) # create index for subsetting dataframe
+calc.bg.rate <- function(df, timecol = 1, bgcol = 2, plot = T) {
+  df <- select(urchin2013, 1, bgcol)  # subset dataframe
+  n <- c(2:length(df))           # create index for subsetting dataframe
   # create individual dataframe subsets:
   bgsubs <- lapply(n, function(x) df[, c(1, x)])
   reps <- c(1:length(bgsubs)) # create another index for calcRate
   calc <- lapply(reps, function (x) calc.rate(bgsubs[[x]], plot = F)$average)
-  plots <- lapply(reps, function (x) calc.rate(bgsubs[[x]], plot = F)$plot)
+  #plots <- lapply(reps, function (x) calc.rate(bgsubs[[x]], plot = F)$plot)
   # extract results:
   results <- as.vector(do.call(rbind, calc))
   average <- mean(results)
   sd <- sd(results)
-  bgout <- list(plots = plots, results = results, average = average, sd = sd)
+  # generate output
+  bgout <- list(results = results, average = average, sd = sd)
   class(bgout) <- 'calc.bg.rate'
-  return(invisible(bgout))
+  # check if plot is TRUE
+  if (plot == T) {
+    pardefault <- par(no.readonly = T)  # save original par settings
+    par(mfrow = n2mfrow(length(results)),
+      oma = c(5,4,0,0) + 0.1,
+      mar = c(0,0,1,1) + 0.1)  # replace par settings
+    lapply(bgsubs, function(x) sub.p(x, title = F))
+    par(pardefault)  # revert par settings to original
+  }
+  return(bgout)
 }
 
 
@@ -34,9 +47,10 @@ print.calc.bg.rate <- function(x) {
   results <- x$results
   average <- x$average
   sd <- sd(results)
-
   cat("Results\n")
   print(results)
-  cat("Average, SD\n")
-  print(c(average, sd))
+  if (length(x$results) > 1){
+    cat("Average, SD\n")
+    print(c(average, sd))
+  }
 }
