60f09a95bf0d0cf02a3c2fba2cc83972840ebfde
Januar Harianto <januar.harianto@sydney.edu.au>
2017-10-30 19:39:50 +1100

    static.roll: improved output to include rsq

--- R/static.roll.R
+++ R/static.roll.R
@@ -1,6 +1,9 @@
 static.roll <- function(df, width) {
-  x <- roll::roll_lm(matrix(df[[1]]), matrix(df[[2]]), width)$coefficients
-  out <- data.table::data.table(na.omit(x))
-  colnames(out) <- c("b0", "b1")
-  return(out[,1:2])
+  x <- roll::roll_lm(matrix(df[[1]]), matrix(df[[2]]), width)
+  out <- na.omit(cbind(x$coefficients,x$r.squared))
+  out <- data.table::data.table(out)
+  data.table::setnames(out, 1:3, c("intercept", "rate", "rsq"))
+  return(out)
 }
+
+
