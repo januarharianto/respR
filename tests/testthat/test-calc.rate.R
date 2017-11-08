b466a7e32a21cdd635df2d9a1c82fe63f8dc5bdf
Januar Harianto <januar.harianto@sydney.edu.au>
2017-11-08 15:02:14 +1100

    testthat: added initial tests

---
+++ tests/testthat/test-calc.rate.R
@@ -0,0 +1,11 @@
+context("calc.rate")
+
+test_that("function works using default arguments", {
+    expect_equal(calc.rate(sardine)$rate, -0.0007280136)
+  })
+test_that("non-df input for x will give an error message", {
+  expect_error(calc.rate("sardine"), "Input must be a data.frame object.")
+})
+test_that("non-numeric inputs of `from` and `to` will give error messages", {
+  expect_error(calc.rate(sardine, from = NULL, to = 4000))
+})
