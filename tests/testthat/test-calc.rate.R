f19f83ca47599960b18ff786fdbeb122d86e9a5e
Januar Harianto <januar.harianto@sydney.edu.au>
2017-11-10 00:21:02 +1100

    more tests

--- tests/testthat/test-calc.rate.R
+++ tests/testthat/test-calc.rate.R
@@ -1,4 +1,4 @@
-context("Testing calc.rate")
+context("calc.rate")
 
 test_that("function works using default arguments", {
     expect_equal(calc.rate(sardine)$rate, -0.0007280136)
@@ -8,6 +8,9 @@ test_that("non-df input for x will give an error message", {
     "Input must be a data.frame object.")
 })
 test_that("non-numeric inputs of `from` and `to` will give error messages", {
-  expect_error(calc.rate(sardine, from = NULL, to = 4000),
-    "'to' and 'from' arguments must be numeric.")
+  expect_error(calc.rate(sardine, from = NULL, to = 4000))
+})
+test_that("intermittent respirometry works", {
+  expect_equal(nrow(calc.rate(intermittent, c(200,2300,4100), c(1800,3200,4600),
+    by = 'time')$results), 3)
 })
