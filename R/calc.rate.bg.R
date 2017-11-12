f1ff0cdb93a3213f11a1b12686a8917d2d3c6df2
Januar Harianto <januar.harianto@sydney.edu.au>
2017-11-12 21:18:14 +1100

    calc.rate.bg: replaces calc.bg.rate

---
+++ R/calc.rate.bg.R
@@ -0,0 +1,115 @@
+#' Title
+#'
+#' @param x
+#' @param xcol
+#' @param ycol
+#' @param from
+#' @param to
+#' @param by
+#' @param plot
+#'
+#' @importFrom data.table data.table
+#'
+#' @return
+#' @export
+#'
+#' @examples
+calc.rate.bg <- function(x, xcol = 1, ycol = 2, from = NULL,
+  to = NULL, by = "time", plot = T) {
+  # Extract data:
+  dt <- data.table(x[c(xcol, ycol)])
+  # Subset data if needed:
+  if (!is.null(from) && !is.null(to))
+    dt <- subset.data(dt, from, to, by)
+  # Perform lm fit on each column:
+  fit <- lapply(1:length(ycol), function(x) lm(dt[[x + 1]] ~ dt[[1]]))
+  # Extract coefficients:
+  coefs <- sapply(1:length(fit), function(x) coef(fit[[x]]))
+  rownames(coefs) <- c("Intercept", "Rate")
+  # Generate output:
+  bg <- unname(coefs[2, ])
+  out <- list(data = dt, lm = fit, results = coefs, bgrate = bg)
+  class(out) <- "calc.rate.bg"
+  # Plot data:
+  if (plot) plot(out)
+  return(out)
+}
+
+
+
+# print -------------------------------------------------------------------
+
+#' Title
+#'
+#' @param x
+#'
+#' @return
+#' @export
+#'
+#' @examples
+print.calc.rate.bg <- function(x) {
+  cat("Rate(s):\n")
+  print(x$bgrate)
+}
+
+
+
+# plot --------------------------------------------------------------------
+
+#' Title
+#'
+#' @param x
+#'
+#' @return
+#' @export
+#'
+#' @examples
+plot.calc.rate.bg <- function(x) {
+
+  pardefault <- par(no.readonly = T)  # save original par settings
+  par(mfrow = n2mfrow(length(x$bgrate)), mai = c(0.4, 0.4, 0.1, 0.1),
+    ps = 10, cex = 1, cex.main = 1)  # replace par settings
+  lapply(1:length(x$bgrate), function(z) sub.p(data.frame(x$data[[1]],
+    x$data[[z + 1]]), title = F))
+  par(pardefault)  # revert par settings to original
+
+}
+
+
+
+# subset.data -------------------------------------------------------------
+
+#' Title
+#'
+#' @param x
+#' @param from
+#' @param to
+#' @param by
+#'
+#' @return
+#' @export
+#'
+#' @examples
+subset.data <- function(x, from, to, by) {
+  dt <- data.table::as.data.table(x)
+  if (by == "time") {
+    out <- dt[dt[[1]] >= from & dt[[1]] <= to]
+  }
+  if (by == "row") {
+    out <- dt[from:to]
+  }
+  if (by == "o2" & length(x) == 2) {
+    top <- Position(function(z) z <= from, dt[[2]])
+    bot <- Position(function(z) z <= to, dt[[2]])
+    out <- dt[top:bot]
+  }
+  if (by == "proportion") {
+    mx <- max(dt[[2]])
+    mn <- min(dt[[2]])
+    top <- Position(function(z) z <= (from * (mx - mn) + mn), dt[[2]])
+    bot <- Position(function(z) z <= (to * (mx - mn) + mn), dt[[2]])
+    out <- dt[top:bot]
+  }
+  return(out)
+}
+
