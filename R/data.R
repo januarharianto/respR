71d98b42fee4096c0f9c391d1c2c64ef6a6c86d9
Januar Harianto <januar.harianto@sydney.edu.au>
2017-11-13 19:48:46 +1100

    new documentation for data objects

---
+++ R/data.R
@@ -0,0 +1,49 @@
+#' Respirometry data of the sea urchin, *Heliocidaris Erythrogramma*
+#'
+#' Multiple measurements of oxygen consumption in a single sea urchin,
+#' *Heliocidaris erythrogramma*, obtained using intermittent flow respirometry.
+#' The experiment was conducted at the Sydney Institute of Marine Science in
+#' Sydney, Australia. There are a total of 3 "loops" where the respirometry
+#' chamber was flushed. Data was collected using a Vernier Optical DO probe
+#' (ODO-BTA).
+#'
+#' @md
+#' @author Nicholas Carey
+#' @format A data frame object consisting of 2 columns (time and dissolved
+#'   oxygen) and 4831 rows (approx 80 min of data).
+"intermittent.rd"
+
+#' Multi-column respirometry data of the sea urchin, *Heliocidaris
+#' Erythrogramma*, including background respiration
+#'
+#' Oxygen consumption data of 16 individual *Heliocidaris erythrogramma*
+#' specimens. In addition, there are 2 measurements of background respiration.
+#' These were trial runs to determine the precision of the new Vernier Optical
+#' DO probes (ODO-BTA).
+#'
+#' @md
+#' @author Januar Harianto
+#' @format A data frame object consisting of one column of time, 16 columns of
+#'   urchin oxygen consumption (a-p) and 2 columns of background oxygen
+#'   consumption (b1 & b2). There are 271 rows of data spanning 45 minutes.
+#'
+#' @source \url{http://www.diamondse.info/}
+"urchins.rd"
+
+#' Respirometry data of the squid, *Doryteuthis opalescens*, with P_crit
+#'
+#'
+#'
+#' @md
+#' @author Nicholas Carey
+#' @format A data frame
+#' @source \url{http://www.diamondse.info/}
+"squid.rd"
+
+#' Respirometry data 0f tye sardine, *Species*
+#'
+#' @md
+#' @author Nicholas Carey
+#' @format A data frame
+#' @source \url{http://www.diamondse.info/}
+"sardine.rd"
