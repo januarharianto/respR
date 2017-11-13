ae129817a14be18262e8067f82e113288d157572
Januar Harianto <januar.harianto@sydney.edu.au>
2017-11-14 10:14:56 +1100

    unit.args: updated the documentation

--- R/unit.args.R
+++ R/unit.args.R
@@ -1,6 +1,28 @@
-#' Print examples for unit arguments in `calc.mo2()`
+#' Print examples of unit arguments for use in [convert.rate()] and
+#' [scale.rate()]
 #'
-#' @md
+#' This is a simple function, requiring no argument. It shows available units
+#' that can be used in the functions [convert.rate()] and [scale.rate()].
+#'
+#' Available units for [convert.rate()]:
+#'
+#' "mg/L", "ug/L", "mmol/L", "umol/L", "mL/L", "mg/kg", "ug/kg", "mmol/kg",
+#' "umol/kg", "mL/kg", "%", "Torr", "hPa", "kPa", "mmHg", "inHg"
+#'
+#' Available units for [scale.rate()]:
+#'
+#' **O2**
+#'
+#' "mg/L", "ug/L", "mmol/L", "umol/L", "mL/L", "mg/kg", "ug/kg", "mmol/kg",
+#' "umol/kg", "mL/kg", "Torr", "hPa", "kPa", "mmHg", "inHg"
+#'
+#' **time**
+#'
+#' "s", "m", "h"
+#'
+#' **mass**
+#'
+#' "ug", "mg", "g", "kg"
 #'
 #' @return A list.
 #' @export
@@ -12,7 +34,10 @@ unit.args <- function() {
   cat("Note: A string-matchin algorithm is used to identify units. e.g. all of these are the same: ")
   cat("mg/L; mg/l, mg L-1, mgL-1, mg per litre, mg.l-1, mg.L-1\n\n")
 
-  o2unit <- c("mg/L", "ug/L", "mmol/L", "umol/L", "mL/L", "mg/kg", "ug/kg", "mmol/kg", "umol/kg", "mL/kg", "%")
+  o2unit <- c("mg/L", "ug/L", "mmol/L", "umol/L", "mL/L", "mg/kg",
+    "ug/kg", "mmol/kg", "umol/kg", "mL/kg", "%", "Torr",
+    "hPa", "kPa", "mmHg", "inHg")
+
   timeunit <- c("s", "m", "h")
   massunit <- c("ug", "mg", "g", "kg")
   cat("O2 Units\n")
