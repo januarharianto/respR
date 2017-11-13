8c37f2df17077d97f3f76e197568056468b723f0
Januar Harianto <januar.harianto@sydney.edu.au>
2017-11-14 10:16:15 +1100

    fix outdated tests

--- tests/testthat/test-calc.rate.R
+++ tests/testthat/test-calc.rate.R
@@ -1,16 +1,18 @@
 context("calc.rate")
 
 test_that("function works using default arguments", {
-    expect_equal(calc.rate(sardine)$rate, -0.0007280136)
+    expect_equal(calc.rate(sardine.rd)$rate, -0.0007280136)
   })
 test_that("non-df input for x will give an error message", {
-  expect_error(calc.rate("sardine"),
+  expect_error(calc.rate("sardine.rd"),
     "Input must be a data.frame object.")
 })
 test_that("non-numeric inputs of `from` and `to` will give error messages", {
-  expect_error(calc.rate(sardine, from = NULL, to = 4000))
+  expect_error(calc.rate(sardine.rd, from = NULL, to = 4000))
 })
 test_that("intermittent respirometry works", {
-  expect_equal(nrow(calc.rate(intermittent, c(200,2300,4100), c(1800,3200,4600),
-    by = 'time')$results), 3)
+  expect_equal(length(calc.rate(intermittent.rd, c(200,2300,4100), c(1800,3200,4600),
+    by = 'time')$rate), 3)
 })
+
+
--- tests/testthat/test-verify.units.R
+++ tests/testthat/test-verify.units.R
@@ -1,19 +1,19 @@
 context("verify.units")
 
 test_that("function can identify one-dimensional O2 unit", {
-  expect_equal(verify.units("ml", "vol")[2], "mL.vol")
+  expect_equal(verify.units("ml", "vol"), "mL.vol")
 })
 
 test_that("function can identify two-dimensional O2 unit", {
-  expect_equal(verify.units("mg/l", "o2")[2], "mg/L.o2")
-  expect_equal(verify.units("mg/kg", "o2")[2], "mg/kg.o2")
+  expect_equal(verify.units("mg/l", "o2"), "mg/L.o2")
+  expect_equal(verify.units("mg/kg", "o2"), "mg/kg.o2")
 })
 
 test_that("function can identify volume unit", {
-  expect_equal(verify.units("ml", "vol")[2], "mL.vol")
+  expect_equal(verify.units("ml", "vol"), "mL.vol")
 })
 
 test_that("function can identify mass unit", {
-  expect_equal(verify.units("mg", "mass")[2], "mg.mass")
+  expect_equal(verify.units("mg", "mass"), "mg.mass")
 })
 
