35db6bb833ff4646d2369c87c14cab04cfc3757f
Januar Harianto <januar.harianto@sydney.edu.au>
2017-11-13 23:01:24 +1100

    static.roll: moved into auto.rate

--- R/static.roll.R
+++
@@ -1,9 +0,0 @@
-static.roll <- function(df, width) {
-  x <- roll::roll_lm(matrix(df[[1]]), matrix(df[[2]]), width)
-  out <- na.omit(cbind(x$coefficients,x$r.squared))
-  out <- data.table::data.table(out)
-  data.table::setnames(out, 1:3, c("intercept_b0", "rate_b1", "rsq"))
-  return(out)
-}
-
-
