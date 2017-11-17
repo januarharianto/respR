69b5e5ab1e974e94e8eeffa2fe4ddfa61c7debc4
Januar Harianto <januar.harianto@sydney.edu.au>
2017-11-18 00:09:58 +1100

    removed test for pcrit (it will come back later)

--- tests/testthat/test-pcrit.R
+++
@@ -1,9 +0,0 @@
-context("pcrit")
-
-test_that("pcrit is of class `pcrit`", {
-  expect_is(pcrit(subsample(squid.rd, 100)), "pcrit")
-})
-
-test_that("pcrit works with default settings", {
-  expect_equal(length(pcrit(subsample(squid.rd, 100))), 10)
-})
