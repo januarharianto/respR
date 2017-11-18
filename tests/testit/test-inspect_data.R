d18ff09e016a1a8b2c2128605e6513a40fc42733
Januar Harianto <januar.harianto@sydney.edu.au>
2017-11-19 10:01:24 +1100

    add test for inspect_data

---
+++ tests/testit/test-inspect_data.R
@@ -0,0 +1,18 @@
+library(testit)
+
+assert(
+  "inspect_data works on 2-column data",
+  suppressMessages(
+    suppressWarnings(inspect_data(sardine.rd)))
+)
+
+
+assert(
+  "inspect_data works on multi-column data",
+  !has_error(suppressMessages(
+    suppressWarnings(
+      capture.output(inspect_data(urchins.rd, plot = F))))),
+  !has_error(suppressMessages(
+    suppressWarnings(
+      capture.output(inspect_data(urchins.rd, 1, 15, plot = F)))))
+)
