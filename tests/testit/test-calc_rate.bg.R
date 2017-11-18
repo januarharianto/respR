0ff747230ea7a19e909cef410a315fe011540b33
Januar Harianto <januar.harianto@sydney.edu.au>
2017-11-19 09:33:48 +1100

    update calc_rate.bg

--- tests/testit/test-calc_rate.bg.R
+++ tests/testit/test-calc_rate.bg.R
@@ -1,1 +1,16 @@
 library(testit)
+
+assert(
+  "calc_rate.bg works using default arguments",
+  !has_error(calc_rate.bg(
+    urchins.rd, xcol = 1, ycol = 18:19, from = 5, to = 45, by = "time",
+    plot = F))
+)
+
+assert(
+  "subset_data returns a data.frame object",
+  is.data.frame(subset_data(sardine.rd, 1000, 2000, by = "time")),
+  is.data.frame(subset_data(sardine.rd, 1000, 2000, by = "row")),
+  is.data.frame(subset_data(sardine.rd, 93, 91, by = "o2")),
+  is.data.frame(subset_data(sardine.rd, 1000, 2000, by = "row"))
+)
