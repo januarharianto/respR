6e2d77656825ecfd0ce12d30e7b0a8625516064b
nicholascarey <nicholascarey@gmail.com>
2017-11-21 16:38:44 +0000

    Edits to vignette and data info docs

--- R/data.R
+++ R/data.R
@@ -3,9 +3,14 @@
 #' Multiple measurements of oxygen consumption in a single sea urchin,
 #' *Heliocidaris erythrogramma*, obtained using intermittent flow respirometry.
 #' The experiment was conducted at the Sydney Institute of Marine Science in
-#' Sydney, Australia. There are a total of 3 "loops" where the respirometry
+#' Sydney, Australia. There are a total of 3 replicates where the respirometry
 #' chamber was flushed. Data was collected using a Vernier Optical DO probe
-#' (ODO-BTA).
+#' (ODO-BTA). 
+#' 
+#' Dissolved oxygen units: mg/L
+#' Time units: seconds
+#' Chamber volume (L): 2.379
+#' Specimen ash-free dry mass (g): 6.955
 #'
 #' @md
 #' @author Nicholas Carey
@@ -22,6 +27,11 @@
 #' specimens. In addition, there are 2 measurements of background respiration.
 #' These were trial runs to determine the precision of the new Vernier Optical
 #' DO probes (ODO-BTA).
+#' 
+#' Dissolved oxygen units: mg/L
+#' Time units: minutes
+#' Chamber volumes (L): ????
+#' Specimen masses (g): ????
 #'
 #' @md
 #' @author Januar Harianto
@@ -35,16 +45,27 @@
 
 #' Respirometry data of the squid, *Doryteuthis opalescens*, with P_crit
 #'
-#'
+#' A single experiment on the squid species *Doryteuthis opalescens* in a 
+#' Loligo Systems swim tunnel and Witrox oxygen probe system. Experiment 
+#' conducted at Hopkins Marine Station, Stanford University, Pacific Grove, 
+#' California.
+#' 
+#' Dissolved oxygen units: mg/L
+#' Time units: seconds
+#' Chamber volume (L): 12.3
+#' Specimen wet mass (g): 21.41
+#' 
+#' Data kindly supplied by Ben Burford, Hopkins Marine Station, 
+#' Stanford University. <bburford@stanford.edu>
 #'
 #' @md
-#' @author Nicholas Carey
+#' @author Ben Burford
 #' @format A data frame
 "squid.rd"
 
 
 
-#' Respirometry data of the sardine, *Species*
+#' Respirometry data of the sardine, *Sardinops sagax*
 #'
 #' @md
 #' @author Nicholas Carey
