815a7f94c7bb9c510fd5b666f2a7882ba26ff3be
Januar Harianto <januar.harianto@sydney.edu.au>
2017-11-29 02:21:36 +1100

    inspect_data: now processes flowthrough
    - new arguments: inflow and outflow
    - changed xcol and ycol to time and oxygen
    - function automatically outputs the right data frame depending on which arguments are filled
    - probably need to improve error message…

--- R/inspect_data.R
+++ R/inspect_data.R
@@ -1,12 +1,12 @@
 #' Check for common errors in respirometry data
 #'
 #' `inspect_data()` scans a data frame for specific errors that may affect the
-#' use of functions in `respR`. Data checks include: 
-#' 
+#' use of functions in `respR`. Data checks include:
+#'
 #' * A test for NA/NaN inputs.
-#' * A test for numeric data. 
-#' * A test for sequential time data. 
-#' * A test for duplicate time data. 
+#' * A test for numeric data.
+#' * A test for sequential time data.
+#' * A test for duplicate time data.
 #' * A test for evenly-spaced time data.
 #'
 #' Once data checks are complete, the function produces a list object which may
@@ -14,9 +14,12 @@
 #' [pcrit()] (NC - i don't think this last one works) for further analyses.
 #'
 #' @md
+#'
 #' @param df data frame. Accepts data frame object of any size.
-#' @param xcol numeric. Defaults to `1`. This is the time data.
-#' @param ycol numaric. Defaults to `2`. This is the dissolved oxygen data.
+#' @param time numeric. Defaults to `1`. This is the time data.
+#' @param oxygen numaric. Defaults to `2`. This is the dissolved oxygen data.
+#' @param inflow
+#' @param outflow
 #' @param highlight logical. Defaults to TRUE. Prints location (row #) of errors
 #'   detected by the function.
 #' @param plot logical. Defaults to TRUE. Produces plots for quick visual
@@ -27,6 +30,7 @@
 #' @import utils
 #' @import stats
 #' @import graphics
+#' @importFrom data.table data.table
 #' @export
 #'
 #' @examples
@@ -37,104 +41,164 @@
 #' # other functions:
 #' calc_rate(inspect_data(sardine.rd, highlight = FALSE, plot = FALSE),
 #'           from = 3000, to = 4000, by = "time")
-inspect_data <- function(df, xcol = 1, ycol = 2, highlight = TRUE,
-  plot = TRUE) {
+inspect_data <- function(df, time = NULL, oxygen = NULL, inflow = NULL,
+  outflow = NULL, highlight = TRUE, plot = TRUE) {
+
   # Validate inputs
-  if (!is.data.frame(df))
-    stop("Test FAILED. `df` input must be a data frame object.")
-  if (!is.numeric(xcol) | !is.numeric(ycol))
-    stop("Test FAILED.'xcol' and 'ycol' inputs must be numeric.")
+  has.time <- is.numeric(time)
+  has.oxygen <- is.numeric(oxygen)
+  has.inflow <- is.numeric(inflow)
+  has.outflow <- is.numeric(outflow)
 
-  # Format data (just in case)
-  df <- data.frame(df)
-  df <- df[c(xcol, ycol)]
-  x <- df[, 1]
-  y <- df[, 2]
-  df.len <- nrow(df)
+  if (!is.data.frame(df)) stop("'df must be data.frame object!")
 
-  # PERFORM CHECKS Rule: if logical result is TRUE, the test is
-  # a FAIL.
+  if (is.null(c(time, oxygen, inflow, outflow))) {
+    # Nothing is specified, subset columns 1 & 2 by default:
+    dt <- data.table::data.table(time = df[[1]], oxygen = df[[2]])
+    type <- "default"
+  } else if (has.time && has.oxygen) {
+    # Time and oxygen columns are specified:
+    dt <- data.table::data.table(time = df[[time]], oxygen = df[[oxygen]])
+    type <- "default"
+  } else if (has.time && has.inflow && has.outflow) {
+    # Time, inflow and outflow columns are specified:
+    dt <- data.table::data.table(time = df[[time]], inflow = df[[inflow]],
+      outflow = df[[outflow]])
+    type <- "flowthrough"
+  } else stop("Input arguments are invalid. Check ?inspect_data if unsure.")
 
-  # Data columns. Are data numeric or a POSIX class?
-  column.check <- test.cols(df)
-  # Convert time to numeric if integer:
-  if (column.check$is.x.integer)
-    df <- column.check$x
-  if (column.check$test.x)
-    stop("Test FAILED. Time column (xcol) must be numeric or POSIX.",
-      call. = F)
-  if (column.check$test.y)
-    stop("Test FAILED. Data column (ycol) must be numeric.",
-      call. = F)
-  # All data. Are there NA/NaN values?
-  na.x <- test_na(x)
-  na.y <- test_na(y)
-  # Time data. Is time not sequential?
-  seq.x <- test_seq(x)
-  # Time data. Are there duplicate time values?
-  dup.x <- test_dupe(x)
-  # Time data. Is time not evenly spaced?
-  equal.x <- test_space(x)
+  # First, convert time to numeric if it is integer
+  if (any(class(dt[,1]) %in% "integer")) dt$Time <- as.numeric(dt$Time)
 
-  if (highlight) {
-    # Print summary
-    print(ls.str(df))
-    cat("---\n")
-    summary <- noquote(rbind(na.x$check, na.y$check, seq.x$check,
-      dup.x$check, equal.x$check))
-    rownames(summary) <- c("No NA/NaN in Time (xcol)", "No NA/NaN in O2 (ycol)",
-      "Sequential Time (xcol)", "Non-duplicated Time",
+  # Check if time is POSIX* if it is, create unique timeseries for checking
+  if (any(class(dt[[1]]) %in% c(c("POSIXct", "POSIXt")))) {
+    time <- as.numeric(dt[[1]])
+  } else time <- dt[[1]]
+
+  # # Create data series for checking
+  # if (type == "default") {
+  #   deet <- dt[[2]]
+  # } else deet <- dt[,-1] # (flowthrough)
+
+  # PERFORM CHECKS -------------------------------------
+  # Rule: if logical result is TRUE, the test is a FAIL.
+
+  # Test NA
+  test_na <- lapply(dt, function(x) check_na(x))
+  time_na <- test_na$time
+  if (type == "default") {
+    oxy_na <- test_na$oxygen
+  } else {
+    inflow_na <- test_na$inflow
+    outflow_na <- test_na$outflow
+  }
+
+  # Test sequential, duplicate and unevenly spaced time
+  time_seq <- check_seq(time)
+  time_dup <- check_dup(time)
+  time_evn <- check_evn(time)
+
+  # Print summary now before we process the checks
+  print(ls.str(df))
+  cat("---\n")
+
+  if (type == "default") {
+    summary <- noquote(rbind(time_na$check, oxy_na$check, time_seq$check,
+      time_dup$check, time_evn$check))
+    rownames(summary) <- c("No NA/NaN in time", "No NA/NaN in Oxygen",
+      "Sequential Time", "Non-duplicated Time",
       "Evenly-spaced Time")
-    colnames(summary) <- "Score"
-    summary[summary == "TRUE"] <- "FAIL***"
-    summary[summary == "FALSE"] <- "PASS"
-    print(summary)
-    cat("---\n")
+  } else {
+    summary <- noquote(rbind(time_na$check, inflow_na$check, outflow_na$check,
+      time_seq$check, time_dup$check, time_evn$check))
+    rownames(summary) <- c("No NA/NaN in time", "No NA/NaN in inflow",
+      "no NA/NaN in outflow", "Sequential Time", "Non-duplicated Time",
+      "Evenly-spaced Time")
   }
 
-  if (na.x$check) {
-    warning("Time column (xcol) contains NA or NaN data.",
-      call. = F)
+  colnames(summary) <- "Score"
+  summary[summary == "TRUE"] <- "FAIL***"
+  summary[summary == "FALSE"] <- "PASS"
+  print(summary)
+
+  # Process checks -------------------------------------
+
+  if (time_na$check) {
+    warning("time column contains NA or NaN data.", call. = F)
     if (highlight)
-      cat("NA/NaN location(s) in time (xcol), by row:\n",
-        na.x$highlight, "\n")
+      cat("NA/NaN location(s) in time, by row:\n",
+        time_na$which, "\n")
   }
-  if (na.y$check) {
-    warning("O2 column (ycol) contains NA or NaN data.",
-      call. = F)
-    if (highlight)
-      cat("NA/NaN location(s) in O2 (ycol), by row:\n",
-        na.y$highlight, "\n")
+
+  if (hasArg(oxy_na)) {
+    if (oxy_na$check) {
+      warning("dissolved oxygen column contains NA or NaN data.", call. = F)
+      if (highlight)
+        cat("NA/NaN location(s) in time, by row:\n",
+          oxy_na$which, "\n")
+    }
   }
-  if (seq.x$check) {
+
+  if (hasArg(inflow_na)) {
+    if (inflow_na$check) {
+      warning("inflow data contains NA/NaN.", call. = F)
+      if (highlight)
+        cat("NA/NaN location(s) in inflow, by row:\n",
+          inflow_na$which, "\n")
+    }
+  }
+
+  if (hasArg(outflow_na)) {
+    if (outflow_na$check) {
+      warning("outflow data contains NA/NaN.", call. = F)
+      if (highlight)
+        cat("NA/NaN location(s) in outflo, by row:\n",
+          outflow_na$which, "\n")
+    }
+  }
+
+  if (time_seq$check) {
     if (highlight)
-      cat("Non-sequential time (xcol) location(s), by row:\n",
-        seq.x$highlight, "\n")
-    stop("Test FAILED. Time data (xcol) are not sequential.",
+      cat("Non-sequential time location(s), by row:\n",
+        time_seq$which, "\n")
+    stop("Test FAILED. Time data are not sequential.",
       " Please check the order of the data.", call. = F)
   }
-  if (dup.x$check) {
+
+  if (time_dup$check) {
     if (highlight)
-      cat("Duplicate time data (xcol) location(s), by row:\n",
-        dup.x$highlight, "\n")
+      cat("Duplicate time data location(s), by row:\n",
+        time_dup$which, "\n")
     stop("Test FAILED. Duplicate time data (xcol) detected.",
       call. = F)
   }
-  if (equal.x$check) {
-    warning("Time data (xcol) is irregular. Subsetting by `row` may result in irregular durations.",
-      call. = F)
+
+  if (time_evn$check) {
+    warning("Time data is irregular. ",
+      "Subsetting by `row` may result in irregular durations.", call. = F)
     if (highlight)
-      if(length(equal.x$highlight) > 50) {
-        cat("Unevenly-spaced time (xcol) location(s), by row (first 50):\n",
-          head(equal.x$highlight, 50), "\n")
+      if(length(time_evn$which) > 50) {
+        cat("More than 50 irregular time intervals detected,",
+          " showing first 20 (by row):\n",
+          head(time_evn$which, 20), "\n")
       } else {
-      cat("Unevenly-spaced time (xcol) location(s), by row:\n",
-        equal.x$highlight, "\n")
+        cat("Unevenly-spaced time location(s), by row:\n",
+          time_evn$which, "\n")
       }
   }
+
+  # Collate highlights
+  if (highlight) {
+    if (type == "default") {
+      highlights <- list(time_na$which, oxy_na$which, time_seq$which,
+        time_dup$which, time_evn$which)
+    } else highlights <- list(time_na$which, inflow_na$which, outflow_na$which,
+      time_seq$which, time_dup$which, time_evn$which)
+  }
+
   # Finally, if df is to be generated, remove NA
-  if (na.x$check | na.y$check) {
-    df <- na.omit(df)  # remove NA rows
+  if (any(is.na(dt))) {
+    df <- na.omit(dt)  # remove NA rows
     warning("Rows containing NA values have been automatically removed.",
       call. = F)
   }
@@ -144,126 +208,74 @@ inspect_data <- function(df, xcol = 1, ycol = 2, highlight = TRUE,
   if (plot) {
     # Calculate rolling regression
     roll <- static_roll(df, floor(0.1 * nrow(df)))$rate_b1
-
-    # # Perform rolling mean
-    # rmean <- roll::roll_mean(matrix(df[,2]), floor(0.2 * nrow(df)))
-    # rmean <- na.omit(rmean)
-    # mr.df <- data.frame(rmean, roll = abs(roll))
-    # mr.df <- dplyr::arrange(mr.df, rmean)
-
-
     pardefault <- par(no.readonly = T)  # save original par settings
     par(mfrow = c(2, 1), mai = c(0.4, 0.4, 0.3, 0.3), ps = 10,
       cex = 1, cex.main = 1)
-    plot(df, xlab = "", ylab = "", col = r1, pch = 16, panel.first = c(rect(par("usr")[1],
-      par("usr")[3], par("usr")[2], par("usr")[4], col = r3),
-      grid(col = "white", lty = 1, lwd = 1.5)))
+    plot(df[[1]], df[[2]], xlab = "", ylab = "", col = r1, pch = 16,
+      panel.first = c(rect(par("usr")[1],
+        par("usr")[3], par("usr")[2], par("usr")[4], col = r3),
+        grid(col = "white", lty = 1, lwd = 1.5)))
     title(main = "Full Timeseries", line = 0.3)
     plot(abs(roll), xlab = "", ylab = "", col = r1, pch = 16,
       panel.first = c(rect(par("usr")[1], par("usr")[3],
         par("usr")[2], par("usr")[4], col = r3), grid(col = "white",
-        lty = 1, lwd = 1.5)))
+          lty = 1, lwd = 1.5)))
     title(main = "Rolling Regression of Rate vs Index (Row No.)",
       line = 0.3)
-    # plot(mr.df, col = r2, pch = 16, xlab = "", ylab = "", lwd = 2, panel.first = c(rect(par("usr")[1],
-    #   par("usr")[3], par("usr")[2], par("usr")[4], col = r3), grid(col = "white", lty = 1,
-    #     lwd = 1.5)))
     par(pardefault)  # revert par settings to original
   }
-  out <- list(df = df, check.columns = column.check, check.na.x = na.x,
-    check.na.y = na.y, check.sequential = seq.x, check.dupe = dup.x,
-    check.uneven.spacing = equal.x)
-
+  out <- list(df = dt, highlights = highlights)
 
   class(out) <- "inspect_data"
   return(invisible(out))
 }
 
 
-# These functions are only used for `inspect_data()`, and will not be visible to
-# the default user.
 
-# test column classes
-test.cols <- function(x) {
-  class.xcol <- class(x[, 1])
-  class.ycol <- class(x[, 2])
-  # first check if time is integer, because we can fix it
-  is.x.integer <- any(class(x[, 1]) == "integer")
-  if (is.x.integer) {
-    warning("Time column (xcol) is of class `integer' and has been converted to `numeric`.",
-      call. = F)
-    cnames <- names(x)
-    convx <- as.numeric(x[, 1])
-    x <- data.frame(convx, x[, 2])
-    names(x) <- cnames
-  }
-  # check that xcol is numeric or date/time
-  test.x <- !any(class(x[, 1]) %in% c("numeric", "POSIXct", "POSIXt"))
-  test.y <- !is.numeric(x[, 2])
-  out <- list(x = x, class.xcol = class.xcol, class.ycol = class.ycol,
-    is.x.integer = is.x.integer, test.x = test.x, test.y = test.y)
-  return(invisible(out))
-}
-
-# Test for NA/NaN
-test_na <- function(x) {
+# check for NA values
+check_na <- function(x) {
   test <- is.na(x)
   check <- any(test)
   highlight <- which(test)
-  out <- list(test = test, check = check, highlight = highlight)
-  return(invisible(out))
+  out <- list(check = check, which = highlight)
+  return(out)
 }
 
-# Test for sequential time
-test_seq <- function(x) {
+# Check for sequential (monotonic) data
+check_seq <- function(x) {
   test <- diff(x) < 0
   test <- ifelse(is.na(test), FALSE, test)  # convert NA values to FALSE
   check <- any(test)
   highlight <- which(test)
-  out <- list(test = test, check = check, highlight = highlight)
-  return(invisible(out))
+  out <- list(check = check, which = highlight)
+  return(out)
 }
 
-
-#' Test for duplicate time
-#'
-#' This is an internal function.
-#'
-#' @keywords internal
-#' @export
-test_dupe <- function(x) {
+# Check for duplicate data
+check_dup <- function(x) {
   test <- x %in% unique(x[duplicated(x, incomparables = NA)])
   check <- any(test)
   highlight <- which(test)
-  out <- list(test = test, check = check, highlight = highlight)
-  return(invisible(out))
+  out <- list(check = check, which = highlight)
+  return(out)
 }
 
-# Calculate mode value in a list
+# Calculate mode
 calc_mode <- function(x) {
   ux <- unique(x)
   ux[which.max(tabulate(match(x, ux)))]
 }
 
-
-#' Test for evenly-spaced time
-#'
-#' This is an internal function.
-#'
-#' @keywords internal
-#' @export
-test_space <- function(x) {
+check_evn <- function(x) {
   spacing <- diff(as.numeric(x))
   mod <- calc_mode(spacing)
 
   test <- spacing != mod
-  # If spacing is even, there should only be 1 interval
-  # detected:
+  # If spacing is even, there should only be 1 interval detected:
   check <- length(unique(spacing)) > 1
 
   test <- ifelse(is.na(test), TRUE, test)  # convert NA values to FALSE
   highlight <- which(test)
-  out <- list(test = test, check = check, highlight = highlight)
-  return(invisible(out))
+  out <- list(check = check, which = highlight)
+  return(out)
 }
-
