c6afbd4af890d2282658d227170071ac202bc765
Januar Harianto <januar.harianto@sydney.edu.au>
2017-11-30 01:49:10 +1100

    modified input arguments for clarity

--- R/calc_rate.ft.R
+++ R/calc_rate.ft.R
@@ -1,7 +1,7 @@
 #' Calculate flowthrough rate of change in dissolved oxygen
 #'
 #' Calculates rate of $O_2$ uptake in flowthrough respirometry given a flow-rate
-#' and both inflow and outflow oxygen concentrations.
+#' and both inflow.o2 and outflow.o2 oxygen concentrations.
 #'
 #' Can return a single value, or multiple and mean values based on continuous
 #' data.
@@ -11,10 +11,10 @@
 #' @param time numeric. Defaults to NULL. This selects the time column if
 #'   a data frame ('df') is provided. Otherwise, this is a numeric vector for
 #'   time data.
-#' @param inflow numeric. Defaults to NULL. This selects the incurrent column if
+#' @param inflow.o2 numeric. Defaults to NULL. This selects the incurrent column if
 #'   a data frame ('df') is provided. Otherwise, this is a numeric vector for
 #'   incurrent oxygen concentration.
-#' @param outflow numeric. Defaults to NULL. This selects the outcurrent column
+#' @param outflow.o2 numeric. Defaults to NULL. This selects the outcurrent column
 #'   if a data frame ('df') is provided.  Otherwise, this is a numeric vector
 #'   for extcurrent oxygen concentration.
 #' @param flowrate numeric vector. The flow rate. No unit of measurement is
@@ -26,13 +26,13 @@
 #'
 #' @examples
 #' NULL
-calc_rate.ft <- function(x = NULL, time = NULL, inflow = NULL, outflow = NULL,
+calc_rate.ft <- function(x = NULL, time = NULL, inflow.o2 = NULL, outflow.o2 = NULL,
   flowrate = NULL, plot = TRUE) {
 
   # Validate inputs
   if (!is.numeric(flowrate)) stop("numeric 'flowrate' value must be provided.")
-  if (is.data.frame(x) && is.null(inflow) && is.null(outflow))
-    stop("You must provide subset locations (row number) for 'inflow' and 'outflow'.")
+  if (is.data.frame(x) && is.null(inflow.o2) && is.null(outflow.o2))
+    stop("You must provide subset locations (row number) for 'inflow.o2' and 'outflow.o2'.")
 
   # Extract individual data
   if (any(class(x) %in% "inspect_data")) {
@@ -40,51 +40,51 @@ calc_rate.ft <- function(x = NULL, time = NULL, inflow = NULL, outflow = NULL,
     # Object is of class `inspect_data`. Extract data from object.
     df      <- x$df
     time    <- df$time
-    inflow  <- df$inflow
-    outflow <- df$outflow
+    inflow.o2  <- df$inflow.o2
+    outflow.o2 <- df$outflow.o2
     message("object of class `inspect_data` detected.")
-    delta <- inflow - outflow
+    delta <- inflow.o2 - outflow.o2
 
   } else if (is.data.frame(x) && is.null(time)) {
 
     # Object is data frame. Time is not provided. Extract data from data frame.
     df <- x
-    inflow <- df[[inflow]]
-    outflow <- df[[outflow]]
-    message("'data.frame' object detected. Values in 'inflow' and 'outflow' are used as column indices to to extract data from the data frame.")
-    delta <- inflow - outflow
+    inflow.o2 <- df[[inflow.o2]]
+    outflow.o2 <- df[[outflow.o2]]
+    message("'data.frame' object detected. Values in 'inflow.o2' and 'outflow.o2' are used as column indices to to extract data from the data frame.")
+    delta <- inflow.o2 - outflow.o2
 
   } else if (is.data.frame(x) && !is.null(time)) {
 
     # Object is data frame. Time is provided. Extract data from data frame.
     df <- x
     time <- df[[time]]
-    inflow <- df[[inflow]]
-    outflow <- df[[outflow]]
-    message("'data.frame' object detected. Values in 'time', inflow' and 'outflow' are used as column indices to extract data from the data frame.")
-    delta <- outflow - inflow
+    inflow.o2 <- df[[inflow.o2]]
+    outflow.o2 <- df[[outflow.o2]]
+    message("'data.frame' object detected. Values in 'time', inflow.o2' and 'outflow.o2' are used as column indices to extract data from the data frame.")
+    delta <- outflow.o2 - inflow.o2
 
-  } else if (is.null(x) && is.null(time) && is.numeric(inflow) &&
-      is.numeric(outflow)) {
+  } else if (is.null(x) && is.null(time) && is.numeric(inflow.o2) &&
+      is.numeric(outflow.o2)) {
 
     # No data frame is provided. Time is not provided.
-    df <- data.table::data.table(inflow = inflow, outflow = outflow)
-    message("calculating from numeric rate values provided in 'inflow' and 'outflow'.")
-    delta <- outflow - inflow
+    df <- data.table::data.table(inflow.o2 = inflow.o2, outflow.o2 = outflow.o2)
+    message("calculating from numeric rate values provided in 'inflow.o2' and 'outflow.o2'.")
+    delta <- outflow.o2 - inflow.o2
 
-  } else if (is.null(x) && !is.null(time) && is.numeric(inflow) &&
-      is.numeric(outflow)) {
+  } else if (is.null(x) && !is.null(time) && is.numeric(inflow.o2) &&
+      is.numeric(outflow.o2)) {
 
     # No data frame is provided. Time is provided. Essentiall same as above.
-    df <- data.table::data.table(time = time, inflow = inflow, outflow = outflow)
-    message("calculating from numeric rate values provided in 'inflow' and 'outflow'.")
-    delta <- outflow - inflow
+    df <- data.table::data.table(time = time, inflow.o2 = inflow.o2, outflow.o2 = outflow.o2)
+    message("calculating from numeric rate values provided in 'inflow.o2' and 'outflow.o2'.")
+    delta <- outflow.o2 - inflow.o2
 
-  } else if ((is.null(inflow) && !is.null(outflow)) |
-      (!is.null(inflow) && is.null(outflow))) {
+  } else if ((is.null(inflow.o2) && !is.null(outflow.o2)) |
+      (!is.null(inflow.o2) && is.null(outflow.o2))) {
 
-    # If inflow is provided, outflow should be provided too. And vice versa.
-    stop("Both 'inflow' and 'outflow' inputs should have a value.")
+    # If inflow.o2 is provided, outflow.o2 should be provided too. And vice versa.
+    stop("Both 'inflow.o2' and 'outflow.o2' inputs should have a value.")
 
   } else stop("Unable to process data. Please double check input arguments.")
 
@@ -94,7 +94,7 @@ calc_rate.ft <- function(x = NULL, time = NULL, inflow = NULL, outflow = NULL,
 
   # Generate summary
   input <- df
-  summary <-  data.table::data.table(inflow, outflow, flowrate, rate)
+  summary <-  data.table::data.table(inflow.o2, outflow.o2, flowrate, rate)
 
   # Generate output
   out <-
