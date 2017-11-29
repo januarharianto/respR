2a970547f95f3ba492663c6f99e9aa44fb712e94
Januar Harianto <januar.harianto@sydney.edu.au>
2017-11-30 09:25:36 +1100

    calc_rate.ft: fix incorrect subset code

--- R/calc_rate.ft.R
+++ R/calc_rate.ft.R
@@ -44,8 +44,8 @@ calc_rate.ft <- function(x = NULL, time = NULL, inflow.o2 = NULL, outflow.o2 = N
     # Extract data from object.
     df      <- x$df
     time    <- df$time
-    inflow.o2  <- df$inflow.o2
-    outflow.o2 <- df$outflow.o2
+    inflow.o2  <- df$inflow
+    outflow.o2 <- df$outflow
     message("object of class `inspect_data` detected.")
     delta <- inflow.o2 - outflow.o2
 
