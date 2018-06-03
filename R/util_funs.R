8fd2162016975a387ba79703352a7cecb2f107d0
Januar Harianto <januar.harianto@sydney.edu.au>
2018-06-04 07:13:25 +1000

    update global variable bindings

--- R/util_funs.R
+++ R/util_funs.R
@@ -1,7 +1,8 @@
 # Deal with pesky "no visible binding for global variable.." checks
 x = NULL; endtime = NULL; rate_b1 = NULL; row.len = NULL; time.len = NULL
 rowlength = NULL; endrow = NULL; timelength = NULL; rate_twopoint = NULL
-endoxy = NULL; oxy = NULL; sumRSS = NULL; do = NULL; y = NULL; V1 = NULL
+endoxy = NULL; oxy = NULL; sumRSS = NULL; do = NULL; y = NULL; V1 = NULL;
+..xcol = NULL; ..ycol = NULL
 
 
 # check os
