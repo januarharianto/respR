ff81486a4a9447b40d3d98b86b85be0f70b96966
Januar Harianto <januar.harianto@sydney.edu.au>
2019-07-10 14:14:31 +0800

    archive current documentation in gh-pages

--- tests/testit/test-calc_rate.bg.R
+++
@@ -1,8 +0,0 @@
-library(testit)
-
-assert(
-  "calc_rate.bg works using default arguments",
-  !has_error(calc_rate.bg(
-    urchins.rd, xcol = 1, ycol = 18:19, from = 5, to = 45, by = "time",
-    plot = F))
-)
