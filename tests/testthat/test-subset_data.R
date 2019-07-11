10466c1da32cd1f88d1b189457f4dd9b7deb397c
nicholascarey <nicholascarey@gmail.com>
2019-07-11 11:41:17 +0100

    subset_data - fix for failure with inspect objects. Updated docs and tests too.

--- tests/testthat/test-subset_data.R
+++ tests/testthat/test-subset_data.R
@@ -1,4 +1,4 @@
-sink("/dev/null") ## stops printing outputs on assigning 
+sink("/dev/null") ## stops printing outputs on assigning
 
 ## works with and outputs df input
 sub <- (subset_data(sardine.rd, from = 2000, to = 3000, by = "time"))
@@ -32,13 +32,25 @@ expect_error(subset_data(sardine.rd, from = 0.8, to = 0.6, by = "p"), regexp = N
 })
 
 ## Error with wrong by
-expect_error(subset_data(sardine.rd, by = "tttimmmeee"), 
+expect_error(subset_data(sardine.rd, by = "tttimmmeee"),
              "`by` input not recognised")
 
 
 ## adjust_rate can be printed
 expect_output(print(sub))
 
+## works with inspect objects for each method
+urch <- inspect(urchins.rd, time = 1, oxygen = 14:15)
+expect_error(subset_data(urch, from = 7.6, to = 7.4, by = "oxygen"), regexp = NA)
+expect_error(subset_data(urch, from = 10, to = 30, by = "time"), regexp = NA)
+expect_error(subset_data(urch, from = 70, to = 170, by = "row"), regexp = NA)
+expect_error(subset_data(urch, from = 0.8, to = 0.4, by = "prop"), regexp = NA)
+
+## includes all columns when subsetting
+expect_equal(ncol((subset_data(urch, from = 7.6, to = 7.4, by = "oxygen"))$dataframe), 3)
+expect_equal(ncol((subset_data(urch, from = 10, to = 30, by = "time"))$dataframe), 3)
+expect_equal(ncol((subset_data(urch, from = 70, to = 170, by = "row"))$dataframe), 3)
+expect_equal(ncol((subset_data(urch, from = 0.8, to = 0.4, by = "prop"))$dataframe), 3)
 
 sink() ## turns printing back on
 
