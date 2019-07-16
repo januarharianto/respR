c8b27830f05d12a588e971b4aa0ec99cafa00f1c
nicholascarey <nicholascarey@gmail.com>
2019-07-16 22:38:45 +0100

    auto_rate - cover 1

--- tests/testthat/test-auto_rate.R
+++ tests/testthat/test-auto_rate.R
@@ -1,5 +1,29 @@
+## accepts inspect and inspect_data objects
+insp_d <- suppressWarnings(inspect_data(sardine.rd))
+expect_error(auto_rate(insp_d), regexp = NA)
 
-## aauto_rate works using default arguments
+insp <- inspect(sardine.rd)
+expect_error(auto_rate(insp), regexp = NA)
+
+## stops if input is not a df (or inspect type object)
+expect_error(auto_rate(as.matrix(sardine.rd)),
+             regexp = "`df` must be a data frame object.")
+
+## uses first two columns of larger df
+## message
+df3col <- cbind(sardine.rd, sardine.rd)
+expect_message(auto_rate(df3col),
+               regexp = "auto_rate: Large dataframe detected. Selecting first 2 columns by default.")
+## subsets only 2 columns
+ar <- auto_rate(df3col)
+expect_equal(ncol(ar$df), 2)
+
+## stops with malformed method
+## For some reason can't get the actual message to match here
+expect_error(auto_rate(sardine.rd, method = "wrong"),
+             regexp = NULL)
+
+## auto_rate works using default arguments
 ar <- auto_rate(sardine.rd, parallel = F, plot = F)
 expect_is(ar,
           "auto_rate")
@@ -67,4 +91,13 @@ expect_error(auto_rate(sardine.rd, parallel = F, plot = F, by = "r"), regexp = N
 ## Error with wrong by
 expect_error(auto_rate(sardine.rd, parallel = F, plot = F, by = "o2"),
              "Invalid `by`` input value, must be 'time' or 'row'.")
+
+
+
+## works with method == "interval" and by == "time"
+int_tm <- auto_rate(sardine.rd, parallel = F, plot = F, method = "interval", by = "Time")
+expect_equal(int_tm$rate[1],
+             -0.0009244829)
+
+
 
