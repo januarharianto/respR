9764f35f08bb93c2b556417028be8618d3212ff6
nicholascarey <nicholascarey@gmail.com>
2019-07-16 23:42:14 +0100

    auto_rate - cover 2

--- tests/testthat/test-auto_rate.R
+++ tests/testthat/test-auto_rate.R
@@ -1,17 +1,17 @@
 ## accepts inspect and inspect_data objects
-insp_d <- suppressWarnings(inspect_data(sardine.rd))
+insp_d <- suppressWarnings(inspect_data(urchins.rd))
 expect_error(auto_rate(insp_d), regexp = NA)
 
-insp <- inspect(sardine.rd)
+insp <- inspect(urchins.rd)
 expect_error(auto_rate(insp), regexp = NA)
 
 ## stops if input is not a df (or inspect type object)
-expect_error(auto_rate(as.matrix(sardine.rd)),
+expect_error(auto_rate(as.matrix(urchins.rd)),
              regexp = "`df` must be a data frame object.")
 
 ## uses first two columns of larger df
 ## message
-df3col <- cbind(sardine.rd, sardine.rd)
+df3col <- cbind(urchins.rd, urchins.rd)
 expect_message(auto_rate(df3col),
                regexp = "auto_rate: Large dataframe detected. Selecting first 2 columns by default.")
 ## subsets only 2 columns
@@ -20,11 +20,11 @@ expect_equal(ncol(ar$df), 2)
 
 ## stops with malformed method
 ## For some reason can't get the actual message to match here
-expect_error(auto_rate(sardine.rd, method = "wrong"),
+expect_error(auto_rate(urchins.rd, method = "wrong"),
              regexp = NULL)
 
 ## auto_rate works using default arguments
-ar <- auto_rate(sardine.rd, parallel = F, plot = F)
+ar <- auto_rate(urchins.rd, parallel = F, plot = F)
 expect_is(ar,
           "auto_rate")
 expect_is(ar$rate,
@@ -33,71 +33,82 @@ expect_is(ar$df,
           "data.frame")
 
 ## auto_rate works by default with `max` argument
-ar <- auto_rate(sardine.rd, parallel = F, plot = F, method = "max")
+ar <- auto_rate(urchins.rd, parallel = F, plot = F, method = "max")
 expect_is(ar,
           "auto_rate")
-ar <- auto_rate(sardine.rd, parallel = F, plot = F, method = "max", width = 7500, by = "time")
+ar <- auto_rate(urchins.rd, parallel = F, plot = F, method = "max", width = 10, by = "time")
 expect_is(ar,
           "auto_rate")
 
 ## auto_rate works by default with `min` argument
-ar <- auto_rate(sardine.rd, parallel = F, plot = F, method = "min")
+ar <- auto_rate(urchins.rd, parallel = F, plot = F, method = "min")
 expect_is(ar,
           "auto_rate")
-ar <- auto_rate(sardine.rd, parallel = F, plot = F, method = "min", width = 7500, by = "time")
+ar <- auto_rate(urchins.rd, parallel = F, plot = F, method = "min", width = 10, by = "time")
 expect_is(ar,
           "auto_rate")
 
 
 ## auto_rate will perform interval method using default values
-ar <- auto_rate(sardine.rd, method = "interval", plot = F, parallel = F)
+ar <- auto_rate(urchins.rd, method = "interval", plot = F, parallel = F)
 expect_is(ar,
           "auto_rate")
 
 ## auto_rate can be plotted
       ## NB - This saves a Rplots.pdf to testthat directory
-      ## Don't know why - something to do with new plots
+      ## Don't know why
       ## Can't see another way of doing this
 expect_error(plot(ar), regex = NA)
 suppressWarnings(file.remove("Rplots.pdf"))
+## and for differnet methods too
+ar <- auto_rate(urchins.rd, method = "max", plot = F, parallel = F)
+expect_error(plot(ar), regex = NA)
+suppressWarnings(file.remove("Rplots.pdf"))
 
-## auto_rate can be printed
+## can be printed for different methods
+ar <- auto_rate(urchins.rd, method = "linear", plot = F, parallel = F)
+expect_output(print(ar))
+ar <- auto_rate(urchins.rd, method = "max", plot = F, parallel = F)
+expect_output(print(ar))
+ar <- auto_rate(urchins.rd, method = "max", plot = F, parallel = F)
+expect_output(print(ar))
+ar <- auto_rate(urchins.rd, method = "interval", plot = F, parallel = F)
 expect_output(print(ar))
 
-## auto_rate can print summary
+## can print summary
 expect_output(summary(ar))
 
 ## static_roll (auto_rate) outputs a data frame object
-sroll <- static_roll(sardine.rd, 1500)
+sroll <- static_roll(urchins.rd, 1500)
 expect_is(sroll,
           "data.frame")
 
 ## time_roll (auto_rate) produces data.frame object",
-troll <- time_roll(sardine.rd, width = 7500, parallel = FALSE)
+troll <- time_roll(urchins.rd, width = 10, parallel = FALSE)
 expect_is(troll,
           "data.frame")
 
 ## time_lm (auto_rate) produces data.frame object",
-tlm <- time_lm(sardine.rd, 10, 50)
+tlm <- time_lm(urchins.rd, 10, 50)
 expect_is(tlm,
           "data.frame")
 
 
 ## Works with variations of `by` input
-expect_error(auto_rate(sardine.rd, parallel = F, plot = F, by = "Time"), regexp = NA)
-expect_error(auto_rate(sardine.rd, parallel = F, plot = F, by = "T"), regexp = NA)
-expect_error(auto_rate(sardine.rd, parallel = F, plot = F, by = "Row"), regexp = NA)
-expect_error(auto_rate(sardine.rd, parallel = F, plot = F, by = "r"), regexp = NA)
+expect_error(auto_rate(urchins.rd, parallel = F, plot = F, by = "Time"), regexp = NA)
+expect_error(auto_rate(urchins.rd, parallel = F, plot = F, by = "T"), regexp = NA)
+expect_error(auto_rate(urchins.rd, parallel = F, plot = F, by = "Row"), regexp = NA)
+expect_error(auto_rate(urchins.rd, parallel = F, plot = F, by = "r"), regexp = NA)
 ## Error with wrong by
-expect_error(auto_rate(sardine.rd, parallel = F, plot = F, by = "o2"),
+expect_error(auto_rate(urchins.rd, parallel = F, plot = F, by = "o2"),
              "Invalid `by`` input value, must be 'time' or 'row'.")
 
 
 
 ## works with method == "interval" and by == "time"
-int_tm <- auto_rate(sardine.rd, parallel = F, plot = F, method = "interval", by = "Time")
+int_tm <- auto_rate(urchins.rd, parallel = F, plot = F, method = "interval", by = "Time")
 expect_equal(int_tm$rate[1],
-             -0.0009244829)
+             -0.0335646)
 
 
 
