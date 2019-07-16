3e78246af3938d0340c9df8eb349e41035c6cb02
nicholascarey <nicholascarey@gmail.com>
2019-07-16 18:25:00 +0100

    adjust_rate cover 2

--- tests/testthat/test-adjust_rate.R
+++ tests/testthat/test-adjust_rate.R
@@ -1,13 +1,16 @@
 
 ## accepts calc_rate.bg inputs
 bg <- calc_rate.bg(urchins.rd, time = 1, oxygen = 18:19)
-expect_error(adjust_rate(100, bg), regexp = NA)
-
 
 ## accepts numeric inputs
 adj <- adjust_rate(100, 20)
 expect_is(adj$input,
           "numeric")
+
+## stops with non-numeric or non calc_rate.bg object
+not_bg <- inspect(urchins.rd, time = 1, oxygen = 18:19)
+expect_error(adjust_rate(100, not_bg),
+             regexp = "'by' must be numeric or object of class 'calc_rate.bg'.")
 
 ## adjust_rate works with input of class `auto_rate` or `calc_rate`
 cr <- adjust_rate(calc_rate(sardine.rd, plot = F), 0.001)
@@ -18,6 +21,13 @@ expect_is(ar,
           "adjust_rate")
 
 
-## adjust_rate can be printed
+## adjust_rate can be printed - if adjustment done to calc_rate or auto_rate object
+expect_output(print(cr))
 expect_output(print(ar))
 
+## adjust_rate can be printed - if adjustment done to numeric value
+nr <- adjust_rate(0.1, 0.001)
+expect_output(print(nr))
+
+
+## summary
