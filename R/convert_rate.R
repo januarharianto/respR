ac44664191803f43714602746979c3940c5af318
nicholascarey <nicholascarey@gmail.com>
2020-03-05 11:39:24 +0000

    convert_rate S3 fixes

--- R/convert_rate.R
+++ R/convert_rate.R
@@ -184,10 +184,14 @@ convert_rate <- function(x, o2.unit = NULL, time.unit = NULL,
 #' @export
 print.convert_rate <- function(x, pos = NULL, ...) {
   cat("\n# convert_rate # ------------------------\n")
+
   if (is.null(pos)) {
     cat("Rank/position 1 result shown. To see all results use summary().\n")
     cat("Input:\n")
     print(x$input.rate[1])
+  } else if(pos > length(object$output.rate)) {
+    stop("Invalid 'pos' rank: only ",
+         length(object$output.rate), " rates found.")
   } else if (is.numeric(pos)) {
     cat("Position", pos, "result\n")
     cat("Input:\n")
@@ -214,15 +218,14 @@ print.convert_rate <- function(x, pos = NULL, ...) {
 #' @export
 summary.convert_rate <- function(object, ...) {
   cat("\n# summary.convert_rate # ----------------\n")
-  out <- (data.table(t(c(object$summary,
-    input.o2.unit = object$input.o2.unit,
-    input.time.unit = object$input.time.unit,
-    output.unit = object$output.unit)))
-    )
-  print(out)
-  return(invisible(x))
-}
+  out <- data.table(object$summary,
+                    input.o2.unit = object$input.o2.unit,
+                    input.time.unit = object$input.time.unit,
+                    output.unit = object$output.unit)
 
+  print(out)
+  return(invisible(object))
+}
 
 #' Convert between multipliers of the same unit, e.g. mg to kg
 #'
