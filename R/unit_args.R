5ff725a9480d29f890f4d8d984ba4dc86642e416
nicholascarey <nicholascarey@gmail.com>
2022-12-30 11:47:46 +0000

    unit_args missing mol from docs

--- R/unit_args.R
+++ R/unit_args.R
@@ -43,13 +43,13 @@
 #'
 #' Do *NOT* require `t`, `S` and `P` for conversions:
 #'
-#' - `"mg/L", "ug/L", "mmol/L", "umol/L", "nmol/L", "pmol/L"`
+#' - `"mg/L", "ug/L", "mol/L", "mmol/L", "umol/L", "nmol/L", "pmol/L"`
 #'
 #' Require `t`, `S` and `P` for conversions:
 #'
-#' - `"mL/L", "cm3/L", "mg/kg", "ug/kg", "mmol/kg", "umol/kg", "nmol/kg",
-#' "pmol/kg", "mL/kg",` `"%Air"` (i.e. % Air Saturation), `"%Oxy"` (i.e. %
-#' Oxygen Saturation), `"Torr", "hPa", "kPa",` `"mmHg", "inHg"`
+#' - `"mL/L", "cm3/L", "mg/kg", "ug/kg", "mol/kg", "mmol/kg", "umol/kg",
+#' "nmol/kg", "pmol/kg", "mL/kg",` `"%Air"` (i.e. % Air Saturation),
+#' `"%Oxy"` (i.e. % Oxygen Saturation), `"Torr", "hPa", "kPa",` `"mmHg", "inHg"`
 #'
 #' ## Time units for `time.unit` or as part of `flowrate.unit`:
 #'
